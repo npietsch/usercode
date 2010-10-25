9f0a0e0c9cace7dfb5f912253e588db29ebc5b21
npietsch <>
2010-10-25 13:54:23 +0000

    renamed

---
+++ SUSYAnalysis/SUSYAnalyzer/plugins/testSelector.h
@@ -0,0 +1,34 @@
+#ifndef SUSYAnalysis_SUSYAnalyzer_testSelector_h
+#define SUSYAnalysis_SUSYAnalyzer_testSelector_h
+
+#include "DataFormats/Common/interface/RefVector.h"
+
+#include "CommonTools/UtilAlgos/interface/StringCutObjectSelector.h"
+#include "CommonTools/UtilAlgos/interface/SingleObjectSelector.h"
+#include "CommonTools/UtilAlgos/interface/ObjectSelector.h"
+#include "CommonTools/UtilAlgos/interface/SingleElementCollectionSelector.h"
+
+#include "DataFormats/PatCandidates/interface/Electron.h"
+#include "DataFormats/PatCandidates/interface/Muon.h"
+#include "DataFormats/PatCandidates/interface/Tau.h"
+#include "DataFormats/PatCandidates/interface/Photon.h"
+#include "DataFormats/PatCandidates/interface/Jet.h"
+#include "DataFormats/PatCandidates/interface/MET.h"
+#include "DataFormats/PatCandidates/interface/PFParticle.h"
+#include "DataFormats/PatCandidates/interface/GenericParticle.h"
+#include "DataFormats/PatCandidates/interface/TriggerObjectStandAlone.h"
+#include "DataFormats/PatCandidates/interface/CompositeCandidate.h"
+
+#include "PhysicsTools/PatAlgos/plugins/PATJetSelector.h"
+
+#include <vector>
+
+namespace pat {
+
+  typedef SingleObjectSelector<
+              reco::GenParticleCollection,
+              StringCutObjectSelector<GenericParticle>
+          > PATgenParticlesSelector;
+}
+
+#endif
