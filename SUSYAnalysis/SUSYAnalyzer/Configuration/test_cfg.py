0c558359a0daac7a9e51febb23026c8858a69e14
npietsch <>
2010-10-25 15:23:32 +0000

    file to test string cut parser basedselection of genParticles

--- SUSYAnalysis/SUSYAnalyzer/Configuration/test_cfg.py
+++ SUSYAnalysis/SUSYAnalyzer/Configuration/test_cfg.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("RA4Selection")
+process = cms.Process("test")
 
 ## configure message logger
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
@@ -9,12 +9,13 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 1
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-    '/store/mc/Spring10/LM1/GEN-SIM-RECO/START3X_V26_S09-v1/0026/B27B78AC-1548-DF11-8117-E41F13181AF8.root'
+    'file:/afs/naf.desy.de/user/n/npietsch/public/testfile.root'
+    #'/store/mc/Spring10/LM1/GEN-SIM-RECO/START3X_V26_S09-v1/0026/B27B78AC-1548-DF11-8117-E41F13181AF8.root'
     )
 )
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
+    input = cms.untracked.int32(500)
 )
 
 process.options = cms.untracked.PSet(
@@ -31,54 +32,7 @@ process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.GlobalTag.globaltag = cms.string('GR_R_38X_V8::All')
 
 #-------------------------------------------------
-# PAT configuration
-#-------------------------------------------------
-
-## std sequence for pat
-process.load("PhysicsTools.PatAlgos.patSequences_cff")
-
-#from PhysicsTools.PatAlgos.tools.cmsswVersionTools import run36xOn35xInput
-#run36xOn35xInput(process)
-
-## remove MC matching, photons, taus and cleaning from PAT default sequence
-from PhysicsTools.PatAlgos.tools.coreTools import *
-removeMCMatching(process, ['All'])
-removeSpecificPATObjects(process,
-                         ['Photons','Taus'],
-                         outputInProcess=False)
-removeCleaning(process,
-               outputInProcess=False)
-
-## add PF jets and MET
-from PhysicsTools.PatAlgos.tools.jetTools import addJetCollection
-addJetCollection(process,cms.InputTag('ak5PFJets'),'AK5','PF',
-                 doJTA        = True,
-                 doBTagging   = True,
-                 jetCorrLabel = ('AK5', 'PF'),
-                 doType1MET   = False,
-                 doL1Cleaning = True,
-                 doL1Counters = False,
-                 genJetCollection=None,
-                 doJetID      = True,
-                 ) 
-from PhysicsTools.PatAlgos.tools.metTools import addPfMET
-addPfMET(process, 'PF')
-
-## remove TagInfos from jets
-process.patJets.addTagInfos = False
-process.patJetsAK5PF.addTagInfos = False
-
-## use the correct jet energy corrections
-process.patJetCorrFactors.corrSample = "Spring10"
-process.patJetCorrFactors.sampleType = "ttbar"
-process.patJetCorrFactorsAK5PF.corrSample = "Spring10"
-process.patJetCorrFactorsAK5PF.sampleType = "ttbar"
-
-#calculate impact parameter w.r.t beam spot (instead of primary vertex)
-process.patMuons.usePV = False
-
-#-------------------------------------------------
-# create collections 
+# create selectedGenParticle collection
 #-------------------------------------------------
       
 ## create genParticle collection
@@ -90,16 +44,9 @@ process.selectedGenParticles = selectedGenParticles.clone(src= "genParticles",
                                                           #'abs(daughter(0).pdgId) == 6'
                                                           )
 
-## create jet collection
-from PhysicsTools.PatAlgos.selectionLayer1.jetSelector_cfi import *
-process.goodJets = selectedPatJets.clone(src = 'selectedPatJets',
-                                     cut =
-                                     'abs(eta) < 2.4 &'
-                                     'pt > 30. &'
-                                     'emEnergyFraction > 0.01 &'
-                                     'jetID.fHPD < 0.98 &'
-                                     'jetID.n90Hits > 1'
-                                     )
+#-------------------------------------------------
+# CountFilter
+#-------------------------------------------------
 
 ## select events with gen Particles
 from SUSYAnalysis.SUSYAnalyzer.testCountFilter_cfi import *
@@ -121,9 +68,7 @@ process.analyzeSUSYEvent = analyzeSUSYEvent.clone(met = "patMETs",
 # selection path
 #-------------------------------------------------
 
-process.Selection = cms.Path(process.patDefaultSequence *
-                             process.goodJets *
-                             process.selectedGenParticles *
+process.Selection = cms.Path(process.selectedGenParticles *
                              process.countGenParticles *
                              process.analyzeSUSYEvent
                              )
@@ -131,24 +76,24 @@ process.Selection = cms.Path(process.patDefaultSequence *
 #-------------------------------------------------
 # optional: write patTuple
 #-------------------------------------------------
-#
-#process.EventSelection = cms.PSet(
-#    SelectEvents = cms.untracked.PSet(
-#    SelectEvents = cms.vstring('RA4Selection')
-#    )
-#)
-#
-#process.out = cms.OutputModule("PoolOutputModule",
-#    process.EventSelection,
-#    outputCommands = cms.untracked.vstring('drop *'),
-#    dropMetaData = cms.untracked.string('DROPPED'),
-#    fileName = cms.untracked.string('PATtuple.root')
-#)
-#
-#from PhysicsTools.PatAlgos.patEventContent_cff import *
-#process.out.outputCommands += patEventContentNoCleaning
-#process.out.outputCommands += patExtraAodEventContent
-#from TopQuarkAnalysis.TopEventProducers.tqafEventContent_cff import *
-#process.out.outputCommands += tqafEventContent
-#
-#process.outpath = cms.EndPath(process.out)
+
+## process.EventSelection = cms.PSet(
+##    SelectEvents = cms.untracked.PSet(
+##    SelectEvents = cms.vstring('Selection')
+##    )
+## )
+
+## process.out = cms.OutputModule("PoolOutputModule",
+##    process.EventSelection,
+##    outputCommands = cms.untracked.vstring('drop *'),
+##    dropMetaData = cms.untracked.string('DROPPED'),
+##    fileName = cms.untracked.string('testfile.root')
+## )
+
+## from PhysicsTools.PatAlgos.patEventContent_cff import *
+## process.out.outputCommands += patEventContentNoCleaning
+## process.out.outputCommands += patExtraAodEventContent
+## from TopQuarkAnalysis.TopEventProducers.tqafEventContent_cff import *
+## process.out.outputCommands += tqafEventContent
+
+## process.outpath = cms.EndPath(process.out)
