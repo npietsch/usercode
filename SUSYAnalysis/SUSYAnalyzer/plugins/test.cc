e8bfcef61275c1759087b0c31a97d49865e97857
npietsch <>
2010-10-25 13:53:52 +0000

    deleted

--- SUSYAnalysis/SUSYAnalyzer/plugins/test.cc
+++
@@ -1,6 +0,0 @@
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "PhysicsTools/PatAlgos/plugins/PATObjectSelector.h"
-
-using namespace pat;
-
-DEFINE_FWK_MODULE(PATgenParticlesSelector);
