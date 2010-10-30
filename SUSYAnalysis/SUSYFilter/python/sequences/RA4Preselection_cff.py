5c93ef942e706eaef02d8d0dd78ea5437a9e1afa
npietsch <>
2010-10-30 19:52:07 +0000

    sequence for trigger and cleaning

---
+++ SUSYAnalysis/SUSYFilter/python/sequences/RA4Preselection_cff.py
@@ -0,0 +1,30 @@
+import FWCore.ParameterSet.Config as cms
+
+#from HLTrigger.HLTfilters.hltHighLevelDev_cfi import hltHighLevelDev
+#process.trigger = hltHighLevelDev.clone(HLTPaths = ['HLT_Mu9'], HLTPathsPrescales = [1], andOr = True)
+
+
+from TopAnalysis.TopFilter.sequences.triggerFilter_cff import *
+
+trigger = hltMu9.clone()
+
+scrapingVeto = cms.EDFilter("FilterOutScraping",
+                            applyfilter = cms.untracked.bool(True),
+                            debugOn = cms.untracked.bool(False),
+                            numtrack = cms.untracked.uint32(10),
+                            thresh = cms.untracked.double(0.25)
+                            )
+
+primaryVertexFilter = cms.EDFilter("GoodVertexFilter",
+                                   vertexCollection = cms.InputTag('offlinePrimaryVertices'),
+                                   minimumNDOF = cms.uint32(4) ,
+                                   maxAbsZ = cms.double(24),
+                                   maxd0 = cms.double(2) )
+
+from CommonTools.RecoAlgos.HBHENoiseFilter_cfi import *
+
+preselection = cms.Sequence(trigger *
+                            scrapingVeto *
+                            primaryVertexFilter*
+                            HBHENoiseFilter
+                            )
