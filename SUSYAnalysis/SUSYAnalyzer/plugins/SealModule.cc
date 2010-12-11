f6fb2c5406560ce3f8be5d0003e14250ed784ca1
npietsch <>
2010-12-11 21:37:47 +0000

    added module to analyze SUSYGenEvent

--- SUSYAnalysis/SUSYAnalyzer/plugins/SealModule.cc
+++ SUSYAnalysis/SUSYAnalyzer/plugins/SealModule.cc
@@ -2,3 +2,6 @@
 
 #include "SUSYAnalysis/SUSYAnalyzer/plugins/SUSYAnalyzer.h"
 DEFINE_FWK_MODULE(SUSYAnalyzer);
+
+#include "SUSYAnalysis/SUSYAnalyzer/plugins/SUSYGenEventAnalyzer.h"
+DEFINE_FWK_MODULE(SUSYGenEventAnalyzer);
--- SUSYAnalysis/SUSYAnalyzer/python/SUSYGenEventAnalyzer_cfi.py
+++ SUSYAnalysis/SUSYAnalyzer/python/SUSYGenEventAnalyzer_cfi.py
@@ -3,8 +3,8 @@ import FWCore.ParameterSet.Config as cms
 #
 # module to make simple analyses of SUSY
 #
-analyzeSUSYGenEvent = cms.EDAnalyzer("SUSYGenEventAnalyzer",
-                                     susyGenEvent = cms.InputTag("SUSYGenEvent")
-                                     )
+analyzeSUSYGenEvt = cms.EDAnalyzer("SUSYGenEventAnalyzer",
+                                   susyGenEvent = cms.InputTag("SUSYGenEvent")
+                                   )
 
 
