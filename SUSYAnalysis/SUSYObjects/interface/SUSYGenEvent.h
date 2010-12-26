7065c7dde881786dccedd8a495ff03df69bf9100
npietsch <>
2010-12-26 22:32:44 +0000

    SUSYGenEvent.h

---
+++ SUSYAnalysis/SUSYObjects/interface/SUSYGenEvent.h
@@ -0,0 +1,89 @@
+#ifndef SUSYObjects_SUSYGenEvent_h
+#define SUSYObjects_SUSYGenEvent_h
+
+#include "DataFormats/Candidate/interface/Candidate.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+
+
+
+namespace SUSYDecayID{
+  static const int stable = 2;
+  static const int unfrag = 3;
+  static const int tID    = 6;
+  static const int bID    = 5;
+  static const int glueID = 21;
+  static const int photID = 22;
+  static const int ZID    = 23;
+  static const int WID    = 24;
+  static const int elecID = 11;
+  static const int muonID = 13;
+  static const int tauID  = 15;
+}
+
+namespace Wdecay{
+  /// classification of leptons in the decay channel 
+  /// of the W boson used in several places throughout 
+  /// the package
+  enum LepType {None, Elec, Muon, Tau};
+}
+
+class SUSYGenEvent {
+
+ public:
+
+  /// empty constructor
+  SUSYGenEvent(){};
+  /// default constructor
+  SUSYGenEvent(reco::GenParticleRefProd& decaySubset, reco::GenParticleRefProd& iniSubset, reco::GenParticleRefProd& iniSparticles);
+  /// default destructor
+  virtual ~SUSYGenEvent(){};
+
+  /// return particles of decay chain
+  const reco::GenParticleCollection& particles() const { return *parts_; }
+  /// return particles of initial partons
+  const reco::GenParticleCollection& initialPartons() const { return *initPartons_;}
+
+  /// return radiated gluons from particle with pdgId
+  std::vector<const reco::GenParticle*> radiatedGluons(int pdgId) const;
+
+  /// return number of leptons in the decay chain
+  int numberOfLeptons(bool fromWBoson=true) const;
+  /// return number of leptons in the decay chain
+  int numberOfLeptons(Wdecay::LepType type, bool fromWBoson=true) const;
+  /// return number of b quarks in the decay chain
+  int numberOfBQuarks() const;
+  /// is squark of first or second generation?
+  bool isSquark(const reco::GenParticle & genParticle) const;
+  /// is gluino?
+  bool isGluino(const reco::GenParticle & genParticle) const;
+  /// is gluino decay?
+  bool GluinoDecay() const;
+  /// is squark decay?
+  bool SquarkDecay() const;
+  /// is gluino-gluino decay?
+  bool GluinoGluinoDecay() const;
+  /// is squark-squark decay?
+  bool SquarkSquarkDecay() const;
+  /// is gluino-squark decay?
+  bool GluinoSquarkDecay() const;
+  /// return pdgId of initial sparticle A
+  int decayChainA() const;
+  /// return pdgId of initial sparticle B
+  int decayChainB() const;
+
+  /// get candidate with given pdg id if available; 0 else 
+  const reco::GenParticle* candidate(int id, unsigned int parentId=0) const;
+
+ protected:
+
+  // reference to the top decay chain (has to be kept in the event!)
+  reco::GenParticleRefProd parts_;       
+  // reference to the list of initial partons (has to be kept in the event!)
+  reco::GenParticleRefProd initPartons_;
+   // reference to the list of initial sparticles (has to be kept in the event!)
+  reco::GenParticleRefProd initSparticles_; 
+};
+
+#endif
+
+
