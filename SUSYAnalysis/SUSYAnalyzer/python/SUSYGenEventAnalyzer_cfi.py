eb0dc2725cc296d5d70f04efb383280eda9f8865
npietsch <>
2010-12-27 00:11:21 +0000

    SUSYGenEventAnalyzer_cfi.py

--- SUSYAnalysis/SUSYAnalyzer/python/SUSYGenEventAnalyzer_cfi.py
+++ SUSYAnalysis/SUSYAnalyzer/python/SUSYGenEventAnalyzer_cfi.py
@@ -4,7 +4,9 @@ import FWCore.ParameterSet.Config as cms
 # module to make simple analyses of SUSY
 #
 analyzeSUSYGenEvt = cms.EDAnalyzer("SUSYGenEventAnalyzer",
-                                   susyGenEvent = cms.InputTag("SUSYGenEvent")
+                                   susyGenEvent = cms.InputTag("SUSYGenEvt"),
+                                   initSubset = cms.InputTag("SUSYInitSubset"),
+                                   jets = cms.InputTag("selectedPatJets")
                                    )
 
 
