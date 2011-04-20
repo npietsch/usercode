ccc55b15ca44ee24975f0e1a504b1abd59292a53
npietsch <>
2011-04-20 01:12:03 +0000

    executes Wolfs submit tool

---
+++ SUSYAnalysis/Configuration/jobSubmit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+nafJobSplitter.pl 25 Bjets_LM8_cfg.py 
+nafJobSplitter.pl 23 Bjets_LM3_cfg.py
+nafJobSplitter.pl 23 Bjets_LM1_cfg.py
+nafJobSplitter.pl 106 Bjets_TTJets_cfg.py
+nafJobSplitter.pl 200 Bjets_QCDMu_cfg.py
+nafJobSplitter.pl 98 Bjets_QCDMu2_cfg.py
+nafJobSplitter.pl 152 Bjets_Wjets_cfg.py
+nafJobSplitter.pl 31 Bjets_Zjets_cfg.py
+nafJobSplitter.pl 9 Bjets_GluinoOSET_cfg.py
