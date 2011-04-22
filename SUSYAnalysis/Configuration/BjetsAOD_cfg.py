e1e642114ca0b84b9a89f31600755bc0476eb13b
npietsch <>
2011-04-22 11:16:31 +0000

    basic cfg file to run analysis on AOD

---
+++ SUSYAnalysis/Configuration/BjetsAOD_cfg.py
@@ -0,0 +1,179 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Bjets")
+
+## configure message logger
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+#process.MessageLogger.cerr.threshold = 'INFO'
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
+process.MessageLogger.categories.append('ParticleListDrawer')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(2000),
+    skipEvents = cms.untracked.uint32(0)
+)
+
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(True)
+)
+
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string('Bjets.root')
+                                   )
+
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = cms.string('START311_V2A::All')
+
+
+#-------------------------------------------------
+# PAT configuration
+#-------------------------------------------------
+
+## Standard pat sequence
+process.load("PhysicsTools.PatAlgos.patSequences_cff")
+
+from PhysicsTools.PatAlgos.tools.cmsswVersionTools import run36xOn35xInput
+
+## remove MC matching, photons, taus and cleaning from PAT default sequence
+from PhysicsTools.PatAlgos.tools.coreTools import *
+##removeMCMatching(process, ['All'])
+
+#removeSpecificPATObjects(process,
+#                         ['Photons'],  # 'Tau' has currently been taken out due to problems with tau discriminators
+#                         outputInProcess=False)
+
+#removeCleaning(process,
+#               outputInProcess=False)
+
+process.patJetCorrFactors.payload = 'AK5Calo'
+# For data:
+#process.patJetCorrFactors.levels = ['L2Relative', 'L3Absolute', 'L2L3Residual', 'L5Flavor', 'L7Parton']
+# For MC:
+process.patJetCorrFactors.levels = ['L2Relative', 'L3Absolute']
+#process.patJetCorrFactors.flavorType = "T"
+
+# embed IsoDeposits
+process.patMuons.isoDeposits = cms.PSet(
+    tracker = cms.InputTag("muIsoDepositTk"),
+    ecal    = cms.InputTag("muIsoDepositCalByAssociatorTowers","ecal"),
+    hcal    = cms.InputTag("muIsoDepositCalByAssociatorTowers","hcal"),
+    user    = cms.VInputTag(
+                            cms.InputTag("muIsoDepositCalByAssociatorTowers","ho"),
+                            cms.InputTag("muIsoDepositJets")
+                            ),
+    )
+process.patMuons.usePV = False
+
+## add PF MET
+from PhysicsTools.PatAlgos.tools.metTools import addPfMET
+addPfMET(process, 'PF')
+
+## Add particle flow jets
+from PhysicsTools.PatAlgos.tools.jetTools import *
+
+addJetCollection(process,cms.InputTag('ak5PFJets'),'AK5','PF',
+                 doJTA        = True,
+                 doBTagging   = True,
+                 jetCorrLabel = ('AK5PF', ['L2Relative', 'L3Absolute']),
+                 doType1MET   = False,
+                 doL1Cleaning = False,
+                 doL1Counters = False,
+                 genJetCollection=None,
+                 doJetID      = True,
+                 )
+
+## remove TagInfos from jets to run on AOD
+process.patJets.addTagInfos = False
+
+process.load("ElectroWeakAnalysis.WENu.simpleEleIdSequence_cff")
+
+process.patElectronIDs = cms.Sequence(process.simpleEleIdSequence)
+process.makePatElectrons = cms.Sequence(process.electronMatch*process.patElectronIDs*process.patElectronIsolation*process.patElectrons)
+
+process.patElectrons.addElectronID = cms.bool(True)
+process.patElectrons.electronIDSources = cms.PSet(
+    simpleEleId95relIso= cms.InputTag("simpleEleId95relIso"),
+    simpleEleId90relIso= cms.InputTag("simpleEleId90relIso"),
+    simpleEleId85relIso= cms.InputTag("simpleEleId85relIso"),
+    simpleEleId80relIso= cms.InputTag("simpleEleId80relIso"),
+    simpleEleId70relIso= cms.InputTag("simpleEleId70relIso"),
+    simpleEleId60relIso= cms.InputTag("simpleEleId60relIso"),
+    simpleEleId95cIso= cms.InputTag("simpleEleId95cIso"),
+    simpleEleId90cIso= cms.InputTag("simpleEleId90cIso"),
+    simpleEleId85cIso= cms.InputTag("simpleEleId85cIso"),
+    simpleEleId80cIso= cms.InputTag("simpleEleId80cIso"),
+    simpleEleId70cIso= cms.InputTag("simpleEleId70cIso"),
+    simpleEleId60cIso= cms.InputTag("simpleEleId60cIso"),
+)
+
+#-------------------------------------------------------------------------------------------------------------------------------
+# Load modules to create SUSY Gen Event and TtGenEvent
+#
+# Note: To create the TtGenEvent for non-SM samples, a small modification in the TQAF is needed:
+#       - Checkout TopQuarkAnalysis/TopEventProducers  (for CMSSW_4_1_4: cvs co -r V06-07-11 TopQuarkAnalysis/TopEventProducers)
+#       - replace in the constructor of TopQuarkAnalysis/TopEventProducers/src/TopDecaySubset.cc "kStart" by "kPythia"
+#-------------------------------------------------------------------------------------------------------------------------------
+
+process.load("SUSYAnalysis.SUSYEventProducers.sequences.SUSYGenEvent_cff")
+process.load("TopQuarkAnalysis.TopEventProducers.sequences.ttGenEvent_cff")
+
+#------------------------------------------------------
+# Import modules to filter events on generator level 
+#------------------------------------------------------
+
+from SUSYAnalysis.SUSYEventProducers.producers.SUSYGenEvtFilter_cfi import *
+process.SUSYGenEventFilter = SUSYGenEventFilter.clone(cut="GluinoGluinoDecay")
+
+from TopQuarkAnalysis.TopEventProducers.producers.TtGenEvtFilter_cfi import *
+process.ttGenEventFilter = ttGenEventFilter.clone(cut="isSemiLeptonic")
+
+#-----------------------------------------------------------------
+# Load modules for preselection. Can be configured later
+#-----------------------------------------------------------------
+
+process.load("SUSYAnalysis.SUSYFilter.sequences.Preselection_cff")
+
+#-----------------------------------------------------------------
+# Load modules to create objects and filter events on reco level
+#-----------------------------------------------------------------
+
+# Object Selection
+process.load("SUSYAnalysis.SUSYFilter.sequences.BjetsSelection_cff")
+
+#--------------------------------------------------------
+# Load modules for analysis on generator and reco-level
+#--------------------------------------------------------
+
+process.load("SUSYAnalysis.SUSYAnalyzer.sequences.SUSYBjetsAnalysis_cff")
+process.load("SUSYAnalysis.SUSYAnalyzer.sequences.SUSYLooseBjetsAnalysis_cff")
+
+#-------------------------------------------------
+# Temporary
+#-------------------------------------------------
+
+## produce printout of particle listings (for debugging)
+process.load("TopQuarkAnalysis.TopEventProducers.sequences.printGenParticles_cff")
+
+#-----------------------------------------------------------------
+# Selection paths. Configure your analysis here, if possible
+#-----------------------------------------------------------------
+
+process.Selection1l = cms.Path(process.patDefaultSequence *
+                               process.makeObjects *
+                               process.makeSUSYGenEvt *
+                               #process.SUSYGenEventFilter *
+                               #process.makeGenEvt *
+                               #process.ttGenEventFitler *
+                               process.preselection *
+                               process.oneGoodMuon*
+                               process.fourGoodJets *
+                               process.twoMediumJets *
+                               process.oneTightJet *
+                               process.metSelection *
+                               process.HTSelection *
+                               process.analyzeSUSYBjets1l_1 *
+                               process.ZVetoMu *
+                               process.analyzeSUSYBjets1l_2
+                               )
