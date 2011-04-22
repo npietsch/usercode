231751ed0d5bfae2c7b152e09b7c61390d25c5ef
npietsch <>
2011-04-22 20:07:48 +0000

    update

--- SUSYAnalysis/Configuration/copy.sh
+++ SUSYAnalysis/Configuration/copy.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
+cp naf_Bjets_LM12_cfg/Bjets.root $1/Bjets_LM12.root
+cp naf_Bjets_LM9_cfg/Bjets.root $1/Bjets_LM9.root
 cp naf_Bjets_LM8_cfg/Bjets.root $1/Bjets_LM8.root
+cp naf_Bjets_LM7_cfg/Bjets.root $1/Bjets_LM7.root
 cp naf_Bjets_LM3_cfg/Bjets.root $1/Bjets_LM3.root
 cp naf_Bjets_LM1_cfg/Bjets.root $1/Bjets_LM1.root
+
+cp naf_Bjets_GluinoOSET400_cfg/Bjets.root $1/Bjets_GluinoOSET400.root
+cp naf_Bjets_GluinoOSET450_cfg/Bjets.root $1/Bjets_GluinoOSET450.root
+cp naf_Bjets_GluinoOSET500_cfg/Bjets.root $1/Bjets_GluinoOSET500.root
+cp naf_Bjets_GluinoOSET550_cfg/Bjets.root $1/Bjets_GluinoOSET550.root
+cp naf_Bjets_GluinoOSET600_cfg/Bjets.root $1/Bjets_GluinoOSET600.root
+cp naf_Bjets_GluinoOSET650_cfg/Bjets.root $1/Bjets_GluinoOSET650.root
+cp naf_Bjets_GluinoOSET700_cfg/Bjets.root $1/Bjets_GluinoOSET700.root
+cp naf_Bjets_GluinoOSET750_cfg/Bjets.root $1/Bjets_GluinoOSET750.root
+cp naf_Bjets_GluinoOSET800_cfg/Bjets.root $1/Bjets_GluinoOSET800.root
+
 cp naf_Bjets_TTJets_cfg/Bjets.root $1/Bjets_TTJets.root
-cp naf_Bjets_QCDMu_cfg/Bjets.root ./Bjets_QCDMu1.root
-cp naf_Bjets_QCDMu2_cfg/Bjets.root ./Bjets_QCDMu2.root
-hadd Bjets_QCDMu.root Bjets_QCDMu?.root
-cp naf_Bjets_QCDMu_cfg/Bjets.root $1/Bjets_QCDMu.root
 cp naf_Bjets_Wjets_cfg/Bjets.root $1/Bjets_Wjets.root
 cp naf_Bjets_Zjets_cfg/Bjets.root $1/Bjets_Zjets.root
 
+cp naf_Bjets_QCDMu_cfg/Bjets.root ./Bjets_QCDMu1.root
+cp naf_Bjets_QCDMu2_cfg/Bjets.root ./Bjets_QCDMu2.root
+hadd Bjets_QCDMu.root Bjets_QCDMu?.root
+cp Bjets_QCDMu.root $1/Bjets_QCDMu.root
 rm Bjets_QCDMu.root
 rm Bjets_QCDMu1.root
 rm Bjets_QCDMu2.root
+
+cp naf_Bjets_MuAB1_cfg/Bjets.root ./Bjets_MuAB1.root
+cp naf_Bjets_MuB2_cfg/Bjets.root ./Bjets_MuB2.root
+hadd Bjets_Mu.root Bjets_Mu*.root
+cp Bjets_Mu.root $1/Bjets_Mu.root
+rm Bjets_Mu.root
+rm Bjets_MuAB1.root
+rm Bjets_MuB2.root
