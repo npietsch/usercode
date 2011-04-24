d4063b9637650c57abadc7457d258f27be080c51
npietsch <>
2011-04-24 19:11:22 +0000

    update

--- SUSYAnalysis/Configuration/jobSubmit.sh
+++ SUSYAnalysis/Configuration/jobSubmit.sh
@@ -8,8 +8,8 @@ nafJobSplitter.pl 23 Bjets_LM3_cfg.py
 nafJobSplitter.pl 23 Bjets_LM1_cfg.py
 nafJobSplitter.pl 106 Bjets_TTJets_cfg.py
 
-#nafJobSplitter.pl 200 Bjets_QCDMu_cfg.py
-#nafJobSplitter.pl 98 Bjets_QCDMu2_cfg.py
+nafJobSplitter.pl 200 Bjets_QCDMu_cfg.py
+nafJobSplitter.pl 98 Bjets_QCDMu2_cfg.py
 nafJobSplitter.pl 152 Bjets_Wjets_cfg.py
 nafJobSplitter.pl 31 Bjets_Zjets_cfg.py
 
@@ -23,5 +23,5 @@ nafJobSplitter.pl 1 Bjets_GluinoOSET700_cfg.py
 nafJobSplitter.pl 1 Bjets_GluinoOSET750_cfg.py
 nafJobSplitter.pl 1 Bjets_GluinoOSET800_cfg.py
 
-#nafJobSplitter.pl 181 Bjets_MuAB1_cfg.py
-#nafJobSplitter.pl 225 Bjets_MuB2_cfg.py
+nafJobSplitter.pl 181 Bjets_MuAB1_cfg.py
+nafJobSplitter.pl 225 Bjets_MuB2_cfg.py
