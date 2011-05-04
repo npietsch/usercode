4c48a89c81ffd39f6d0feccd51582d5f5e777815
npietsch <>
2011-05-04 15:11:18 +0000

    update

---
+++ SUSYAnalysis/Configuration/Bjets_GluinoOSET500_cfg.py
@@ -0,0 +1,8 @@
+from BjetsAOD_cfg import *
+
+# Choose input files
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+    'file:/scratch/hh/lustre/cms/user/npietsch/job1_gluino0j500.1.lhe_7TeV_GEN_FASTSIM.root'
+    )
+)
