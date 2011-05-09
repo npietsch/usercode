25e55a023b9a94304634509d16169f4be98a7a62
npietsch <>
2011-05-09 09:25:11 +0000

    update

--- SUSYAnalysis/Configuration/Run2011/Bjets_MuHad2_v2_cfg.py
+++ SUSYAnalysis/Configuration/Run2011/Bjets_MuHad2_v2_cfg.py
@@ -1,5 +1,8 @@
 from BjetsData_cfg import *
 
+from SUSYAnalysis.SUSYFilter.sequences.Preselection_cff import *
+process.preselection = preselectionMuHTData
+
 # Choose input files
 process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring(      
