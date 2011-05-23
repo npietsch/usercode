7be036e9b10f8e7b71e6c098cc6609121ff98f5b
npietsch <>
2011-05-23 12:07:09 +0000

    update

---
+++ SUSYAnalysis/Configuration/Run2011/RA4Synch_cfg.py
@@ -0,0 +1,94 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("RA4Synch")
+
+## configure message logger
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+#process.MessageLogger.cerr.threshold = 'INFO'
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
+process.MessageLogger.categories.append('ParticleListDrawer')
+
+# Choose input files
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+    '/store/user/npietsch/LM1_SUSY_sftsht_7TeV-pythia6/PAT/89614fbe8472c25559618ceb16e3da73/Spring11_10_1_BhC.root',
+    '/store/user/npietsch/LM1_SUSY_sftsht_7TeV-pythia6/PAT/89614fbe8472c25559618ceb16e3da73/Spring11_11_1_Le5.root',
+    '/store/user/npietsch/LM1_SUSY_sftsht_7TeV-pythia6/PAT/89614fbe8472c25559618ceb16e3da73/Spring11_12_1_KZa.root',
+    '/store/user/npietsch/LM1_SUSY_sftsht_7TeV-pythia6/PAT/89614fbe8472c25559618ceb16e3da73/Spring11_13_1_3uv.root',
+    '/store/user/npietsch/LM1_SUSY_sftsht_7TeV-pythia6/PAT/89614fbe8472c25559618ceb16e3da73/Spring11_14_1_viA.root',
+    '/store/user/npietsch/LM1_SUSY_sftsht_7TeV-pythia6/PAT/89614fbe8472c25559618ceb16e3da73/Spring11_15_1_4k1.root',
+    '/store/user/npietsch/LM1_SUSY_sftsht_7TeV-pythia6/PAT/89614fbe8472c25559618ceb16e3da73/Spring11_16_1_9x4.root',
+    '/store/user/npietsch/LM1_SUSY_sftsht_7TeV-pythia6/PAT/89614fbe8472c25559618ceb16e3da73/Spring11_17_1_9eE.root',
+    '/store/user/npietsch/LM1_SUSY_sftsht_7TeV-pythia6/PAT/89614fbe8472c25559618ceb16e3da73/Spring11_18_1_75D.root',
+    '/store/user/npietsch/LM1_SUSY_sftsht_7TeV-pythia6/PAT/89614fbe8472c25559618ceb16e3da73/Spring11_19_1_jk5.root',
+    '/store/user/npietsch/LM1_SUSY_sftsht_7TeV-pythia6/PAT/89614fbe8472c25559618ceb16e3da73/Spring11_1_1_aVA.root',
+    '/store/user/npietsch/LM1_SUSY_sftsht_7TeV-pythia6/PAT/89614fbe8472c25559618ceb16e3da73/Spring11_20_1_ucC.root',
+    '/store/user/npietsch/LM1_SUSY_sftsht_7TeV-pythia6/PAT/89614fbe8472c25559618ceb16e3da73/Spring11_21_1_tz2.root',
+    '/store/user/npietsch/LM1_SUSY_sftsht_7TeV-pythia6/PAT/89614fbe8472c25559618ceb16e3da73/Spring11_22_1_wGO.root',
+    '/store/user/npietsch/LM1_SUSY_sftsht_7TeV-pythia6/PAT/89614fbe8472c25559618ceb16e3da73/Spring11_2_1_LNw.root',
+    '/store/user/npietsch/LM1_SUSY_sftsht_7TeV-pythia6/PAT/89614fbe8472c25559618ceb16e3da73/Spring11_3_1_Evm.root',
+    '/store/user/npietsch/LM1_SUSY_sftsht_7TeV-pythia6/PAT/89614fbe8472c25559618ceb16e3da73/Spring11_4_1_4cW.root',
+    '/store/user/npietsch/LM1_SUSY_sftsht_7TeV-pythia6/PAT/89614fbe8472c25559618ceb16e3da73/Spring11_5_1_NoR.root',
+    '/store/user/npietsch/LM1_SUSY_sftsht_7TeV-pythia6/PAT/89614fbe8472c25559618ceb16e3da73/Spring11_6_1_szm.root',
+    '/store/user/npietsch/LM1_SUSY_sftsht_7TeV-pythia6/PAT/89614fbe8472c25559618ceb16e3da73/Spring11_7_1_0vR.root',
+    '/store/user/npietsch/LM1_SUSY_sftsht_7TeV-pythia6/PAT/89614fbe8472c25559618ceb16e3da73/Spring11_8_1_uS1.root',
+    '/store/user/npietsch/LM1_SUSY_sftsht_7TeV-pythia6/PAT/89614fbe8472c25559618ceb16e3da73/Spring11_9_1_KMj.root'
+    )
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1000),
+    skipEvents = cms.untracked.uint32(0)
+)
+
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(True)
+)
+
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string('Bjets.root')
+                                   )
+
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = cms.string('START311_V2A::All')
+
+
+#-----------------------------------------------------------------
+# Load modules for preselection
+#-----------------------------------------------------------------
+
+process.load("SUSYAnalysis.SUSYFilter.sequences.Preselection_cff")
+
+#-----------------------------------------------------------------
+# Load modules to create objects and filter events on reco level
+#-----------------------------------------------------------------
+
+# Object Selection
+process.load("SUSYAnalysis.SUSYFilter.sequences.RA4_Selection_cff")
+process.load("SUSYAnalysis.SUSYFilter.sequences.MuonID_cff")
+
+#--------------------------------------------------------
+# Load modules for analysis on generator and reco-level
+#--------------------------------------------------------
+
+## process.load("SUSYAnalysis.SUSYAnalyzer.sequences.SUSYBjetsAnalysis_cff")
+
+#-----------------------------------------------------------------
+# Selection paths. Configure your analysis here, if possible
+#-----------------------------------------------------------------
+
+## Muon
+process.MuonSelection = cms.Path(process.makeObjects *
+                                 process.preselectionMuSynch *
+                                 process.oneGoodMuon *
+                                 process.oneVetoMuon *
+                                 process.noVetoElectron *
+                                 process.oneLooseJet*
+                                 process.twoLooseJets *
+                                 process.threeLooseJets *
+                                 process.fourLooseJets *
+                                 process.oneGoodMET
+                                 )
+
---
+++ SUSYAnalysis/SUSYFilter/python/sequences/RA4_Selection_cff.py
@@ -0,0 +1,200 @@
+import FWCore.ParameterSet.Config as cms
+
+#------------------------------
+# lepton collections 
+#------------------------------
+
+## create good muon collection
+from PhysicsTools.PatAlgos.selectionLayer1.muonSelector_cfi import *
+from TopAnalysis.TopFilter.sequences.MuonVertexDistanceSelector_cfi import *
+
+trackMuons = selectedPatMuons.clone(src = "selectedPatMuons",
+                                    cut =
+                                    'isGood("GlobalMuonPromptTight") &'
+                                    'isGood("AllTrackerMuons") &'
+                                    'pt >= 20. &'
+                                    'abs(eta) <= 2.1 &'
+                                    '(trackIso+hcalIso+ecalIso)/pt < 0.1 &'
+                                    'abs(dB) < 0.02 &'
+                                    'globalTrack.hitPattern.numberOfValidTrackerHits > 10 &'
+                                    'numberOfMatches() > 1 &'
+                                    'innerTrack().hitPattern().pixelLayersWithMeasurement() >= 1'
+                                   )
+
+goodMuons = vertexSelectedMuons.clone(src = "trackMuons"
+                                      )
+
+## create good electron collection
+from PhysicsTools.PatAlgos.selectionLayer1.electronSelector_cfi import *
+from SUSYAnalysis.SUSYFilter.sequences.ElectronVertexDistanceSelector_cfi import *
+
+isolatedElectrons = selectedPatElectrons.clone(src = 'selectedPatElectrons',
+                                               cut =
+                                               'pt >= 20. &'
+                                               'abs(eta) <= 2.5 &'
+                                               'electronID(\"simpleEleId80relIso\")=7 &'
+                                               '(abs(eta) < 1.4442 | abs(eta) > 1.566) &'
+                                               'abs(dB) < 0.02 '
+                                               )
+
+goodElectrons = vertexSelectedElectrons.clone(src = "isolatedElectrons"
+                                              )
+
+#------------------------------
+# veto-lepton collections
+#------------------------------
+
+## create veto-muon collection
+from PhysicsTools.PatAlgos.selectionLayer1.muonSelector_cfi import *
+vetoMuons = selectedPatMuons.clone(src = 'selectedPatMuons',
+                                   cut =
+                                   'isGlobalMuon &'
+                                   'pt >= 10. &'
+                                   'abs(eta) <= 2.5 &'
+                                   '(trackIso+hcalIso+ecalIso)/pt <  0.2'
+                                   )
+
+## create veto-electron collection
+from PhysicsTools.PatAlgos.selectionLayer1.electronSelector_cfi import *
+vetoElectrons = selectedPatElectrons.clone(src = 'selectedPatElectrons',
+                                           cut =
+                                           'pt >= 15. &'
+                                           'abs(eta) <= 2.5 &'
+                                           'electronID(\"simpleEleId95relIso\")=7 '
+                                           )
+
+#------------------------------
+# jet collections
+#------------------------------
+
+## create good jet collection
+from PhysicsTools.PatAlgos.selectionLayer1.jetSelector_cfi import *
+looseJets = selectedPatJets.clone(src = 'selectedPatJetsAK5PF',
+                                  cut =
+                                  'abs(eta) < 2.4 &'
+                                  'pt > 30. &'
+                                  # PURE09 LOOSE
+                                  'emEnergyFraction > 0.01 &'
+                                  'jetID.fHPD < 0.98 &'
+                                  'jetID.n90Hits > 1'
+                                  # Loose PF Jet ID
+##                                   'chargedHadronEnergyFraction > 0.0  &'
+##                                   'neutralHadronEnergyFraction < 0.99 &'
+##                                   'chargedEmEnergyFraction     < 0.99 &'
+##                                   'neutralEmEnergyFraction     < 0.99 &'
+##                                   'chargedMultiplicity > 0            &'
+##                                   'nConstituents > 1'
+                                  )
+
+looseJets.checkOverlaps = cms.PSet(
+    muons = cms.PSet(
+    src       = cms.InputTag("goodMuons"),
+    algorithm = cms.string("byDeltaR"),
+    preselection        = cms.string(""),
+    deltaR              = cms.double(0.3),
+    checkRecoComponents = cms.bool(False),
+    pairCut             = cms.string(""),
+    requireNoOverlaps   = cms.bool(True),
+    ),
+    electrons = cms.PSet(
+    src       = cms.InputTag("goodElectrons"),
+    algorithm = cms.string("byDeltaR"),
+    preselection        = cms.string(""),
+    deltaR              = cms.double(0.3),
+    checkRecoComponents = cms.bool(False),
+    pairCut             = cms.string(""),
+    requireNoOverlaps   = cms.bool(True),
+    )
+)
+    
+#------------------------------
+# MET collection
+#------------------------------
+
+## create MET collection
+from PhysicsTools.PatAlgos.selectionLayer1.metSelector_cfi import *
+goodMETs = selectedPatMET.clone(src = 'patMETsPF',
+                                cut =
+                                'et > 100.'
+                                )
+#------------------------------
+# muon countFilter
+#------------------------------
+
+## select events with at least good muon
+from PhysicsTools.PatAlgos.selectionLayer1.muonCountFilter_cfi import *
+oneGoodMuon = countPatMuons.clone(src = 'goodMuons',
+                                  minNumber = 1
+                                  )
+
+#------------------------------
+# electron countFilter
+#------------------------------
+
+## select events with at least one good electron
+from PhysicsTools.PatAlgos.selectionLayer1.electronCountFilter_cfi import *
+oneGoodElectron = countPatElectrons.clone(src = 'goodElectrons',
+                                          minNumber = 1
+                                          )
+#------------------------------
+# veto-lepton countFilter
+#------------------------------
+
+## select events with one veto muon
+from PhysicsTools.PatAlgos.selectionLayer1.muonCountFilter_cfi import *
+oneVetoMuon = countPatMuons.clone(src = 'vetoMuons',
+                                  maxNumber = 1
+                                  )
+
+## select events with no veto electron
+from PhysicsTools.PatAlgos.selectionLayer1.electronCountFilter_cfi import *
+noVetoElectron = countPatElectrons.clone(src = 'vetoElectrons',
+                                         maxNumber = 0
+                                         )
+#------------------------------
+# jet countFilter
+#------------------------------
+
+## select events with 1 loose jet
+from PhysicsTools.PatAlgos.selectionLayer1.jetCountFilter_cfi import *
+oneLooseJet = countPatJets.clone(src = 'looseJets',
+                                 minNumber = 1
+                                 )
+## select events with 2 loose jets
+from PhysicsTools.PatAlgos.selectionLayer1.jetCountFilter_cfi import *
+twoLooseJets = countPatJets.clone(src = 'looseJets',
+                                  minNumber = 2
+                                  )
+## select events with 3 loose jets
+from PhysicsTools.PatAlgos.selectionLayer1.jetCountFilter_cfi import *
+threeLooseJets = countPatJets.clone(src = 'looseJets',
+                                  minNumber = 3
+                                    )
+## select events with 4 loose jets
+from PhysicsTools.PatAlgos.selectionLayer1.jetCountFilter_cfi import *
+fourLooseJets = countPatJets.clone(src = 'looseJets',
+                                   minNumber = 4
+                                           )
+#------------------------------
+# MET countFilter
+#------------------------------
+
+## select events with one good MET
+from PhysicsTools.PatAlgos.selectionLayer1.metCountFilter_cfi import *
+oneGoodMET = countPatMET.clone(src = 'goodMETs',
+                               minNumber = 1
+                               )
+
+#------------------------------
+# Define sequences
+#------------------------------
+
+goodObjects = cms.Sequence(trackMuons *
+                           goodMuons *
+                           vetoMuons *
+                           isolatedElectrons *
+                           goodElectrons *
+                           vetoElectrons *
+                           looseJets *
+                           goodMETS
+                           )
