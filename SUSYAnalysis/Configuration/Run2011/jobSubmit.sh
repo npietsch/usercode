92cd3ce08596de4b3d999bc7e32dbc856bb258e4
npietsch <>
2011-05-29 19:29:37 +0000

    update

--- SUSYAnalysis/Configuration/Run2011/jobSubmit.sh
+++ SUSYAnalysis/Configuration/Run2011/jobSubmit.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-nafJobSplitter.pl 25 Bjets_MuHad_v1_cfg.py
-nafJobSplitter.pl 200 Bjets_MuHad_v2_cfg.py
-nafJobSplitter.pl 95 Bjets_MuHad2_v2_cfg.py
+#nafJobSplitter.pl 25 Bjets_MuHad_v1_cfg.py
+#nafJobSplitter.pl 200 Bjets_MuHad_v2_cfg.py
+#nafJobSplitter.pl 95 Bjets_MuHad2_v2_cfg.py
 
-nafJobSplitter.pl 19 Bjets_LM9_cfg.py
-nafJobSplitter.pl 25 Bjets_LM8_cfg.py
-nafJobSplitter.pl 23 Bjets_LM3_cfg.py
+nafJobSplitter.pl 21 Bjets_LM9_cfg.py
+nafJobSplitter.pl 22 Bjets_LM8_cfg.py
+nafJobSplitter.pl 21 Bjets_LM3_cfg.py
+nafJobSplitter.pl 22 Bjets_LM13_cfg.py
 #nafJobSplitter.pl 23 Bjets_LM1_cfg.py
 nafJobSplitter.pl 130 Bjets_TTJets_cfg.py
 #nafJobSplitter.pl 130 Bjets_TTJetsSemiMuon_cfg.py
 #nafJobSplitter.pl 130 Bjets_TTJetsOther_cfg.py
 
-nafJobSplitter.pl 200 Bjets_QCDMu_cfg.py
-nafJobSplitter.pl 95 Bjets_QCDMu2_cfg.py
-nafJobSplitter.pl 110 Bjets_Wjets_cfg.py
-nafJobSplitter.pl 27 Bjets_Zjets_cfg.py
+nafJobSplitter.pl 218 Bjets_QCDMu_cfg.py
+nafJobSplitter.pl 109 Bjets_Wjets_cfg.py
+nafJobSplitter.pl 213 Bjets_Zjets_cfg.py
 
 #nafJobSplitter.pl 1 Bjets_GluinoOSET400_cfg.py
 #nafJobSplitter.pl 1 Bjets_GluinoOSET450_cfg.py
