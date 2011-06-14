c0bb90dc73ba0be138864594815f900f5a30f798
npietsch <>
2011-06-14 10:41:54 +0000

    update

--- SUSYAnalysis/SUSYFilter/python/sequences/ElectronVertexDistanceSelector_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-vertexSelectedElectrons = cms.EDProducer("ElectronVertexDistanceSelector",
-  src           = cms.InputTag("selectedPatElectrons"),
-  primaryVertex = cms.InputTag("offlinePrimaryVertices")
-)
