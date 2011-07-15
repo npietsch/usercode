7acbb340da14dbd70d989b56f7d4349f235864e7
npietsch <>
2011-07-15 19:49:56 +0000

    update

--- SUSYAnalysis/Configuration/Run2011/Bjets_tbGluinoOSET600_cfg.py
+++
@@ -1,8 +0,0 @@
-from BjetsAOD_cfg import *
-
-# Choose input files
-process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring(
-    'file:/scratch/hh/lustre/cms/user/npietsch/job1_tbgluino0j600.1.lhe_7TeV_GEN_FASTSIM.root'
-    )
-)
