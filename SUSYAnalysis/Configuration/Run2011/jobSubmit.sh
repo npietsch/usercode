7bc36bfce571405357d09a26937e0c11680c8c18
npietsch <>
2011-07-20 08:58:34 +0000

    update

--- SUSYAnalysis/Configuration/Run2011/jobSubmit.sh
+++ SUSYAnalysis/Configuration/Run2011/jobSubmit.sh
@@ -15,15 +15,15 @@ nafJobSplitter.pl 50 Bjets_ElHad2_v4_cfg.py
 #nafJobSplitter.pl 21 Bjets_LM3_cfg.py
 
 nafJobSplitter.pl 43 RA4b_TTJets_cfg.py
-nafJobSplitter.pl 50 RA4b_Wjets_cfg_1.py
-nafJobSplitter.pl 50 RA4b_Wjets_cfg_2.py
-nafJobSplitter.pl 50 RA4b_Wjets_cfg_3.py
-nafJobSplitter.pl 50 RA4b_Wjets_cfg_4.py
-nafJobSplitter.pl 50 RA4b_Wjets_cfg_5.py
+nafJobSplitter.pl 50 RA4b_WJets_cfg_1.py
+nafJobSplitter.pl 50 RA4b_WJets_cfg_2.py
+nafJobSplitter.pl 50 RA4b_WJets_cfg_3.py
+nafJobSplitter.pl 50 RA4b_WJets_cfg_4.py
+nafJobSplitter.pl 50 RA4b_WJets_cfg_5.py
 
-nafJobSplitter.pl 50 RA4b_Zjets_cfg_1.py
-nafJobSplitter.pl 50 RA4b_Zjets_cfg_2.py
-nafJobSplitter.pl 50 RA4b_Zjets_cfg_3.py
+nafJobSplitter.pl 50 RA4b_ZJets_cfg_1.py
+nafJobSplitter.pl 50 RA4b_ZJets_cfg_2.py
+nafJobSplitter.pl 50 RA4b_ZJets_cfg_3.py
 
 nafJobSplitter.pl 50 RA4b_QCDMu_cfg_1.py
 nafJobSplitter.pl 50 RA4b_QCDMu_cfg_2.py
