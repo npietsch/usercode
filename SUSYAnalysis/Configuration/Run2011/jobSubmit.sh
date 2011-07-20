837ed00ae98041532932c5e35f25598074c90fb2
npietsch <>
2011-07-20 01:39:43 +0000

    update

--- SUSYAnalysis/Configuration/Run2011/jobSubmit.sh
+++ SUSYAnalysis/Configuration/Run2011/jobSubmit.sh
@@ -1,38 +1,29 @@
 #!/bin/bash
 
-#nafJobSplitter.pl 25 Bjets_MuHad_v1_cfg.py
-#nafJobSplitter.pl 200 Bjets_MuHad_v2_cfg.py
-#nafJobSplitter.pl 95 Bjets_MuHad2_v2_cfg.py
+nafJobSplitter.pl 50 Bjets_MuHad1_v1_cfg.py
+nafJobSplitter.pl 50 Bjets_MuHad2_v1_cfg.py
+nafJobSplitter.pl 50 Bjets_MuHad1_v4_cfg.py
+nafJobSplitter.pl 50 Bjets_MuHad2_v4_cfg.py
 
-nafJobSplitter.pl 21 Bjets_LM9_cfg.py
-nafJobSplitter.pl 22 Bjets_LM8_cfg.py
-nafJobSplitter.pl 21 Bjets_LM3_cfg.py
-nafJobSplitter.pl 22 Bjets_LM13_cfg.py
-#nafJobSplitter.pl 23 Bjets_LM1_cfg.py
-nafJobSplitter.pl 130 Bjets_TTJets_cfg.py
-#nafJobSplitter.pl 130 Bjets_TTJetsSemiMuon_cfg.py
-#nafJobSplitter.pl 130 Bjets_TTJetsOther_cfg.py
+nafJobSplitter.pl 50 Bjets_ElHad1_v1_cfg.py
+nafJobSplitter.pl 50 Bjets_ElHad2_v1_cfg.py
+nafJobSplitter.pl 50 Bjets_ElHad1_v4_cfg.py
+nafJobSplitter.pl 50 Bjets_ElHad2_v4_cfg.py
 
-nafJobSplitter.pl 218 Bjets_QCDMu_cfg.py
-nafJobSplitter.pl 109 Bjets_Wjets_cfg.py
-nafJobSplitter.pl 213 Bjets_Zjets_cfg.py
+#nafJobSplitter.pl 21 Bjets_LM9_cfg.py
+#nafJobSplitter.pl 22 Bjets_LM8_cfg.py
+#nafJobSplitter.pl 21 Bjets_LM3_cfg.py
 
-#nafJobSplitter.pl 1 Bjets_GluinoOSET400_cfg.py
-#nafJobSplitter.pl 1 Bjets_GluinoOSET450_cfg.py
-#nafJobSplitter.pl 1 Bjets_GluinoOSET500_cfg.py
-#nafJobSplitter.pl 1 Bjets_GluinoOSET550_cfg.py
-#nafJobSplitter.pl 1 Bjets_GluinoOSET600_cfg.py
-#nafJobSplitter.pl 1 Bjets_GluinoOSET650_cfg.py
-#nafJobSplitter.pl 1 Bjets_GluinoOSET700_cfg.py
-#nafJobSplitter.pl 1 Bjets_GluinoOSET750_cfg.py
-#nafJobSplitter.pl 1 Bjets_GluinoOSET800_cfg.py
+nafJobSplitter.pl 43 RA4b_TTJets_cfg.py
+nafJobSplitter.pl 50 RA4b_Wjets_cfg_1.py
+nafJobSplitter.pl 50 RA4b_Wjets_cfg_2.py
+nafJobSplitter.pl 50 RA4b_Wjets_cfg_3.py
+nafJobSplitter.pl 50 RA4b_Wjets_cfg_4.py
+nafJobSplitter.pl 50 RA4b_Wjets_cfg_5.py
 
-#nafJobSplitter.pl 1 Bjets_tbGluinoOSET400_cfg.py
-#nafJobSplitter.pl 1 Bjets_tbGluinoOSET450_cfg.py
-#nafJobSplitter.pl 1 Bjets_tbGluinoOSET500_cfg.py
-#nafJobSplitter.pl 1 Bjets_tbGluinoOSET550_cfg.py
-#nafJobSplitter.pl 1 Bjets_tbGluinoOSET600_cfg.py
-#nafJobSplitter.pl 1 Bjets_tbGluinoOSET650_cfg.py
-#nafJobSplitter.pl 1 Bjets_tbGluinoOSET700_cfg.py
-#nafJobSplitter.pl 1 Bjets_tbGluinoOSET750_cfg.py
-#nafJobSplitter.pl 1 Bjets_tbGluinoOSET800_cfg.py
+nafJobSplitter.pl 50 RA4b_Zjets_cfg_1.py
+nafJobSplitter.pl 50 RA4b_Zjets_cfg_2.py
+nafJobSplitter.pl 50 RA4b_Zjets_cfg_3.py
+
+nafJobSplitter.pl 50 RA4b_QCDMu_cfg_1.py
+nafJobSplitter.pl 50 RA4b_QCDMu_cfg_2.py
