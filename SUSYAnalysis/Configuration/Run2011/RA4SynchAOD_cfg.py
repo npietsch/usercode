40c17e8d4c6e000f1bc62ada373d3a5f4653010a
npietsch <>
2011-07-29 13:32:25 +0000

    update

--- SUSYAnalysis/Configuration/Run2011/RA4SynchAOD_cfg.py
+++ SUSYAnalysis/Configuration/Run2011/RA4SynchAOD_cfg.py
@@ -4,7 +4,7 @@ process = cms.Process("RA4Synch")
 
 ## configure message logger
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
-process.MessageLogger.cerr.FwkReport.reportEvery = 100
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
 process.MessageLogger.categories.append('ParticleListDrawer')
 
 # Choose input files
@@ -30,148 +30,7 @@ process.TFileService = cms.Service("TFileService",
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = cms.string('GR_R_42_V14::All')
-
-#-------------------------------------------------
-# PAT configuration
-#-------------------------------------------------
-
-## load standard pat sequence
-process.load("PhysicsTools.PatAlgos.patSequences_cff")
-
-## dummy output module
-process.out = cms.OutputModule("PoolOutputModule",
-    outputCommands = cms.untracked.vstring('drop *'),
-    dropMetaData = cms.untracked.string("DROPPED"),                                     
-    fileName = cms.untracked.string('Summer11.root')
-)
-
-## remove cleaning as it is not used
-#from PhysicsTools.PatAlgos.tools.coreTools import *
-#removeCleaning(process)
-
-## switch off MC matching
-from PhysicsTools.PatAlgos.tools.coreTools import *
-removeMCMatching(process, ['All'])
-
-## remove tau and photon collection
-#removeSpecificPATObjects(process, ['Taus', 'Photons'])
-
-## use the correct jet energy corrections
-process.patJetCorrFactors.flavorType = "T"
-
-## calculate d0 wrt the beam spot
-process.patMuons.usePV = False
-
-## Add PfMET and TcMET to the event content
-from PhysicsTools.PatAlgos.tools.metTools import *
-addPfMET(process,'PF')
-addTcMET(process,'JPT')
-		
-## add ak5JPTJets
-from PhysicsTools.PatAlgos.tools.jetTools import *
-addJetCollection(process,cms.InputTag('JetPlusTrackZSPCorJetAntiKt5'),'AK5', 'JPT',
-                 doJTA        = True,
-                 doBTagging   = True,
-                 jetCorrLabel = ('AK5JPT', ['L1FastJet', 'L2Relative','L3Absolute']),
-                 doType1MET   = False,
-                 doL1Cleaning = False,
-                 doL1Counters = False,                 
-                 genJetCollection = None,
-                 doJetID      = True
-                )
-		 
-## Add particle flow jets
-addJetCollection(process,cms.InputTag('ak5PFJets'),'AK5','PF',
-                 doJTA        = True,
-                 doBTagging   = True,
-                 jetCorrLabel = ('AK5PF', ['L1FastJet', 'L2Relative','L3Absolute']),
-                 doType1MET   = False,
-                 doL1Cleaning = False,
-                 doL1Counters = False,
-                 genJetCollection=None,
-                 doJetID      = True
-                )
-
-## add L1 offset corrections to Calo Jets
-process.patJetCorrFactors.levels=['L1FastJet', 'L2Relative','L3Absolute']
-
-## fix bug in patDefaultSequence
-process.load('RecoJets.Configuration.RecoPFJets_cff')
-process.kt6PFJets.doRhoFastjet = True
-process.patDefaultSequence.replace(process.patJetCorrFactors,
-                                   process.kt6PFJets + process.patJetCorrFactors)
-
-## embedding of jet constituents into the jets
-process.patJets.embedCaloTowers       = False
-process.patJetsAK5JPT.embedCaloTowers = False
-process.patJetsAK5PF.embedPFCandidates= False
-
-## remove TagInfos from jets
-process.patJets.addTagInfos       = False
-process.patJetsAK5JPT.addTagInfos = False
-process.patJetsAK5PF.addTagInfos  = False
-
-## embed IsoDeposits
-process.patMuons.isoDeposits = cms.PSet(
-     tracker = cms.InputTag("muIsoDepositTk"),
-     ecal    = cms.InputTag("muIsoDepositCalByAssociatorTowers","ecal"),
-     hcal    = cms.InputTag("muIsoDepositCalByAssociatorTowers","hcal"),
-     user    = cms.VInputTag(cms.InputTag("muIsoDepositCalByAssociatorTowers","ho"),
-                             cms.InputTag("muIsoDepositJets")
-                            ),
-    )
-    
-## embedding of resolutions into the patObjects
-process.load("TopQuarkAnalysis.TopObjectResolutions.stringResolutions_etEtaPhi_cff")
-process.patJets.addResolutions = True
-process.patJets.resolutions = cms.PSet(
-default = cms.string("udscResolution"),
-bjets = cms.string("bjetResolution"),
-)
-process.patJetsAK5PF.addResolutions = True
-process.patJetsAK5PF.resolutions = cms.PSet(
-default = cms.string("udscResolutionPF"),
-bjets = cms.string("bjetResolutionPF"),
-)
-process.patElectrons.addResolutions = True
-process.patElectrons.resolutions = cms.PSet( default = cms.string("elecResolution") )
-process.patMuons.addResolutions = True
-process.patMuons.resolutions = cms.PSet( default = cms.string("muonResolution") )
-process.patMETs.addResolutions = True
-process.patMETs.resolutions = cms.PSet( default = cms.string("metResolution") )
-process.patMETsPF.addResolutions = True
-process.patMETsPF.resolutions = cms.PSet( default = cms.string("metResolutionPF") )  
-
-# Turn off photon-electron cleaning (i.e., flag only)
-process.cleanPatPhotons.checkOverlaps.electrons.requireNoOverlaps = False
-
-# Embed tracks, since we drop them
-process.patElectrons.embedTrack = True
-process.patMuons.embedTrack   = True
-
-## add electron identification
-process.load("ElectroWeakAnalysis.WENu.simpleEleIdSequence_cff")
-
-process.patElectrons.electronIDSources = cms.PSet(
-    eidTight = cms.InputTag("eidTight"),
-    eidLoose = cms.InputTag("eidLoose"),
-    eidRobustTight = cms.InputTag("eidRobustTight"),
-    eidRobustHighEnergy = cms.InputTag("eidRobustHighEnergy"),
-    eidRobustLoose = cms.InputTag("eidRobustLoose"),
-    simpleEleId95relIso= cms.InputTag("simpleEleId95relIso"),
-    simpleEleId90relIso= cms.InputTag("simpleEleId90relIso"),
-    simpleEleId85relIso= cms.InputTag("simpleEleId85relIso"),
-    simpleEleId80relIso= cms.InputTag("simpleEleId80relIso"),
-    simpleEleId70relIso= cms.InputTag("simpleEleId70relIso"),
-    simpleEleId60relIso= cms.InputTag("simpleEleId60relIso"),
-    simpleEleId95cIso= cms.InputTag("simpleEleId95cIso"),
-    simpleEleId90cIso= cms.InputTag("simpleEleId90cIso"),
-    simpleEleId85cIso= cms.InputTag("simpleEleId85cIso"),
-    simpleEleId80cIso= cms.InputTag("simpleEleId80cIso"),
-    simpleEleId70cIso= cms.InputTag("simpleEleId70cIso"),
-    simpleEleId60cIso= cms.InputTag("simpleEleId60cIso"))
-process.patDefaultSequence.replace(process.patElectrons,process.simpleEleIdSequence+process.patElectrons)
+#process.GlobalTag.globaltag = cms.string('GR_R_42_V14::All')
 
 #-----------------------------------------------------------------
 # Load modules for preselection
@@ -184,6 +43,80 @@ process.load("SUSYAnalysis.SUSYFilter.sequences.Preselection_cff")
 #-----------------------------------------------------------------
 
 process.load("SUSYAnalysis.SUSYFilter.sequences.BjetsSelection_cff")
+
+## dummy output module
+process.out = cms.OutputModule("PoolOutputModule",
+    outputCommands = cms.untracked.vstring('drop *'),
+    dropMetaData = cms.untracked.string("DROPPED"),                                     
+    fileName = cms.untracked.string('Summer11.root')
+)
+
+#-----------------------------------------------------------------
+# From PhysicsTools/Configuration/test/SUSY_pattuple_cfg.py
+#-----------------------------------------------------------------
+
+#-- VarParsing ----------------------------------------------------------------
+import FWCore.ParameterSet.VarParsing as VarParsing
+options = VarParsing.VarParsing ('standard')
+
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+#options.output = "SUSYPAT.root"
+options.maxEvents = 100
+
+#  for SusyPAT configuration
+options.register('GlobalTag', "GR_R_42_V14::All", VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.string, "GlobalTag to use (if empty default Pat GT is used)")
+options.register('mcInfo', False, VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.int, "process MonteCarlo data")
+options.register('jetCorrections', 'L1FastJet', VarParsing.VarParsing.multiplicity.list, VarParsing.VarParsing.varType.string, "Level of jet corrections to use: Note the factors are read from DB via GlobalTag")
+options.jetCorrections.append('L2Relative')
+options.jetCorrections.append('L3Absolute')
+#options.jetCorrections.append('L2L3Residual')
+options.register('hltName', 'HLT', VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.string, "HLT menu to use for trigger matching")
+options.register('mcVersion', '', VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.string, "Currently not needed and supported")
+options.register('jetTypes', 'AK5PF', VarParsing.VarParsing.multiplicity.list, VarParsing.VarParsing.varType.string, "Additional jet types that will be produced (AK5Calo and AK5PF, cross cleaned in PF2PAT, are included anyway)")
+#options.register('hltSelection', '*', VarParsing.VarParsing.multiplicity.list, VarParsing.VarParsing.varType.string, "hlTriggers (OR) used to filter events")
+options.register('hltSelection', 'HLT_Mu8_HT200_v*', VarParsing.VarParsing.multiplicity.list, VarParsing.VarParsing.varType.string, "hlTriggers (OR) used to filter events")
+options.hltSelection.append('HLT_Mu15_HT200_v*')
+options.register('doValidation', False, VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.int, "Include the validation histograms from SusyDQM (needs extra tags)")
+options.register('doExtensiveMatching', False, VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.int, "Matching to simtracks (needs extra tags)")
+options.register('doSusyTopProjection', False, VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.int, "Apply Susy selection in PF2PAT to obtain lepton cleaned jets (needs validation)")
+options.register('addKeep', '', VarParsing.VarParsing.multiplicity.list, VarParsing.VarParsing.varType.string, "Additional keep and drop statements to trim the event content")
+
+process.maxEvents.input = options.maxEvents
+# Due to problem in production of LM samples: same event number appears multiple times
+process.source.duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
+
+#-- Calibration tag -----------------------------------------------------------
+if options.GlobalTag:
+    process.GlobalTag.globaltag = options.GlobalTag
+
+
+############################# START SUSYPAT specifics ####################################
+from PhysicsTools.Configuration.SUSY_pattuple_cff import addDefaultSUSYPAT, getSUSY_pattuple_outputCommands
+#Apply SUSYPAT
+addDefaultSUSYPAT(process,options.mcInfo,options.hltName,options.jetCorrections,options.mcVersion,options.jetTypes,options.doValidation,options.doExtensiveMatching,options.doSusyTopProjection)
+SUSY_pattuple_outputCommands = getSUSY_pattuple_outputCommands( process )
+############################## END SUSYPAT specifics ####################################
+
+
+#-- TFileService --------------------------------------------------------------
+process.load ( "PhysicsTools.UtilAlgos.TFileService_cfi")
+process.TFileService = cms.Service("TFileService",
+  fileName = cms.string('histo.root')
+)
+
+#-- HLT selection ------------------------------------------------------------
+import HLTrigger.HLTfilters.hltHighLevel_cfi as hlt
+if options.hltSelection:
+    process.hltFilter = hlt.hltHighLevel.clone(
+        HLTPaths = cms.vstring(options.hltSelection),
+        TriggerResultsTag = cms.InputTag("TriggerResults","",options.hltName),
+        throw = False
+    )
+    process.susyPatDefaultSequence.replace(process.eventCountProducer, process.eventCountProducer * process.hltFilter)
+
+process.metJESCorAK5PFTypeI.corrector = cms.string('ak5PFL2L3')
+
+process.p = cms.Path( process.susyPatDefaultSequence )
 
 #-----------------------------------------------------------------
 # Load modules to monitor selection steps
@@ -207,23 +140,9 @@ process.Out.electrons ="goodElectrons"
 
 #------------------
 # Selection paths
-#-------------------
+#------------------
 
-## ## Muon selection
-## process.MuonSelection = cms.Path(process.patDefaultSequence *
-##                                  process.preselectionMuSynchData2 *
-##                                  process.goodObjects *
-##                                  process.fourGoodJets *
-##                                  process.oneGoodMuon *
-##                                  process.exactlyOneGoodMuon *
-##                                  process.noGoodElectron *
-##                                  process.oneVetoMuon *
-##                                  process.noVetoElectron *
-##                                  process.HTSelection *
-##                                  process.oneTightMET
-##                                  )
-
-process.ViennaMuonSelection = cms.Path(process.patDefaultSequence *
+process.ViennaMuonSelection = cms.Path(process.susyPatDefaultSequence *
                                        process.preselectionMuSynchData2 *
                                        process.goodObjects *
                                        process.RA4Preselection *
@@ -244,30 +163,4 @@ process.ViennaMuonSelection = cms.Path(process.patDefaultSequence *
                                        process.oneTightMET *
                                        process.Out
                                        )
-## ## Electron selection
-## process.ElectronSelection = cms.Path(process.patDefaultSequence *
-##                                      process.preselectionElSynchData2 *
-##                                      process.goodObjects *
-##                                      process.fourGoodJets *
-##                                      process.oneGoodElectron *
-##                                      process.exactlyOneGoodElectron *
-##                                      process.noGoodMuon *
-##                                      process.oneVetoElectron *
-##                                      process.noVetoMuon *
-##                                      process.HTSelection *
-##                                      process.oneTightMET
-##                                      )
 
-## process.ViennaElectronSelection = cms.Path(process.patDefaultSequence *
-##                                            process.preselectionElSynchData2 *
-##                                            process.goodObjects *
-##                                            process.fourGoodJets *
-##                                            process.oneGoodElectron *
-##                                            process.exactlyOneGoodElectron *
-##                                            process.noGoodMuon *
-##                                            process.oneVetoElectron *
-##                                            process.noVetoMuon *
-##                                            process.ViennaHTSelection *
-##                                            process.oneTightMET
-##                                            )
- 
