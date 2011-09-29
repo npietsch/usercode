c1c32b0f7854fac22ac2481675de7a3d48eab7de
npietsch <>
2011-09-29 11:43:48 +0000

    *** empty log message ***

---
+++ Btagging/Configuration/jobSubmit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+nafJobSplitter.pl 235 SemiLepTTBar_cfg.py
+nafJobSplitter.pl 235 FullLepTTBar_cfg.py
+nafJobSplitter.pl 235 FullHadTTBar_cfg.py
+nafJobSplitter.pl 235 TauTTBar_cfg.py
+
+nafJobSplitter.pl 200 WJets1_cfg.py
+nafJobSplitter.pl 238 WJets1_cfg.py
