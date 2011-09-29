c1918271ff8a9f8805c34490f8d1dafaa907efe4
npietsch <>
2011-09-29 14:37:57 +0000

    *** empty log message ***

--- Btagging/Configuration/jobSubmit.sh
+++ Btagging/Configuration/jobSubmit.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-nafJobSplitter.pl 235 TTJets_cfg.py
+nafJobSplitter.pl 50 TTJets_cfg.py
 
-nafJobSplitter.pl 235 SemiLepTTBar_cfg.py
-nafJobSplitter.pl 235 FullLepTTBar_cfg.py
-nafJobSplitter.pl 235 FullHadTTBar_cfg.py
-nafJobSplitter.pl 235 TauTTBar_cfg.py
+nafJobSplitter.pl 50 SemiLepTTBar_cfg.py
+nafJobSplitter.pl 50 FullLepTTBar_cfg.py
+nafJobSplitter.pl 50 FullHadTTBar_cfg.py
+nafJobSplitter.pl 50 TauTTBar_cfg.py
 
-nafJobSplitter.pl 200 WJets1_cfg.py
-nafJobSplitter.pl 238 WJets1_cfg.py
+nafJobSplitter.pl 50 WJets1_cfg.py
+nafJobSplitter.pl 50 WJets2_cfg.py
 
-nafJobSplitter.pl 221 MuHad_May10_cfg.py
-nafJobSplitter.pl 220 MuHad_PromptReco1_v4_cfg.py
-nafJobSplitter.pl 190 MuHad_PromptReco2_v4_cfg.py
-nafJobSplitter.pl 103 MuHad_PromptReco2_v5_cfg.py
-nafJobSplitter.pl 169 MuHad_PromptReco2_v5_cfg.py
+nafJobSplitter.pl 50 MuHad_May10_cfg.py
+nafJobSplitter.pl 50 MuHad_PromptReco1_v4_cfg.py
+nafJobSplitter.pl 50 MuHad_PromptReco2_v4_cfg.py
+nafJobSplitter.pl 50 MuHad_PromptReco_v5_cfg.py
+nafJobSplitter.pl 50 MuHad_PromptReco_v6_cfg.py
+
+#nafJobSplitter.pl 235 TTJets_cfg.py
+
+#nafJobSplitter.pl 235 SemiLepTTBar_cfg.py
+#nafJobSplitter.pl 235 FullLepTTBar_cfg.py
+#nafJobSplitter.pl 235 FullHadTTBar_cfg.py
+#nafJobSplitter.pl 235 TauTTBar_cfg.py
+
+#nafJobSplitter.pl 200 WJets1_cfg.py
+#nafJobSplitter.pl 238 WJets2_cfg.py
+
+#nafJobSplitter.pl 221 MuHad_May10_cfg.py
+#nafJobSplitter.pl 220 MuHad_PromptReco1_v4_cfg.py
+#nafJobSplitter.pl 190 MuHad_PromptReco2_v4_cfg.py
+#nafJobSplitter.pl 103 MuHad_PromptReco_v5_cfg.py
+#nafJobSplitter.pl 169 MuHad_PromptReco_v6_cfg.py
