afe28d2d0799bb3ec9c5d08d76488d502afb3ce5
npietsch <>
2011-10-02 17:44:39 +0000

    *** empty log message ***

--- Btagging/Configuration/jobSubmit.sh
+++ Btagging/Configuration/jobSubmit.sh
@@ -7,15 +7,6 @@ nafJobSplitter.pl 50 FullLepTTBar_cfg.py
 nafJobSplitter.pl 50 FullHadTTBar_cfg.py
 nafJobSplitter.pl 50 TauTTBar_cfg.py
 
-#nafJobSplitter.pl 50 WJets1_cfg.py
-#nafJobSplitter.pl 50 WJets2_cfg.py
-
-#nafJobSplitter.pl 50 MuHad_May10_cfg.py
-#nafJobSplitter.pl 50 MuHad_PromptReco1_v4_cfg.py
-#nafJobSplitter.pl 50 MuHad_PromptReco2_v4_cfg.py
-#nafJobSplitter.pl 50 MuHad_PromptReco_v5_cfg.py
-#nafJobSplitter.pl 50 MuHad_PromptReco_v6_cfg.py
-
 #nafJobSplitter.pl 235 TTJets_cfg.py
 
 #nafJobSplitter.pl 235 SemiLepTTBar_cfg.py
@@ -25,6 +16,12 @@ nafJobSplitter.pl 50 TauTTBar_cfg.py
 
 nafJobSplitter.pl 200 WJets1_cfg.py
 nafJobSplitter.pl 238 WJets2_cfg.py
+
+nafJobSplitter.pl 220 DY1_cfg.py
+nafJobSplitter.pl 239 DY2_cfg.py
+
+nafJobSplitter.pl 
+nafJobSplitter.pl
 
 nafJobSplitter.pl 221 MuHad_May10_cfg.py
 nafJobSplitter.pl 220 MuHad_PromptReco1_v4_cfg.py
