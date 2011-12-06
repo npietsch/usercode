8cf0c51a19fa0e91432574ceec6c751bdcad2d96
npietsch <>
2011-12-06 11:48:35 +0000

    update

--- SUSYAnalysis/SUSYObjects/interface/GenEvent.h
+++ SUSYAnalysis/SUSYObjects/interface/GenEvent.h
@@ -5,23 +5,23 @@
 #include "DataFormats/HepMCCandidate/interface/GenParticle.h"
 #include <string>
 
-namespace SUSYDecayID{
-  static const int stable = 2;
-  static const int unfrag = 3;
-  static const int tID    = 6;
-  static const int bID    = 5;
-  static const int glueID = 21;
-  static const int photID = 22;
-  static const int ZID    = 23;
-  static const int WID    = 24;
-  static const int elecID = 11;
-  static const int muonID = 13;
-  static const int tauID  = 15;
-}
+/* namespace SUSYDecayID{ */
+/*   static const int stable = 2; */
+/*   static const int unfrag = 3; */
+/*   static const int tID    = 6; */
+/*   static const int bID    = 5; */
+/*   static const int glueID = 21; */
+/*   static const int photID = 22; */
+/*   static const int ZID    = 23; */
+/*   static const int WID    = 24; */
+/*   static const int elecID = 11; */
+/*   static const int muonID = 13; */
+/*   static const int tauID  = 15; */
+/* } */
 
-namespace Wdecay{
-  enum LepType {None, Elec, Muon, Tau};
-}
+/* namespace Wdecay{ */
+/*   enum LepType {None, Elec, Muon, Tau}; */
+/* } */
 
 class GenEvent {
 
--- SUSYAnalysis/SUSYObjects/src/classes.h
+++ SUSYAnalysis/SUSYObjects/src/classes.h
@@ -1,5 +1,6 @@
 #include "SUSYAnalysis/SUSYObjects/interface/SUSYGenEvent.h"
 #include "SUSYAnalysis/SUSYObjects/interface/SUSYEvent.h"
+#include "SUSYAnalysis/SUSYObjects/interface/GenEvent.h"
 
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "TString.h"
@@ -7,11 +8,11 @@
 namespace {
 
   struct dictionary {
-    //edm::Wrapper<std::vector<std::vector<int> > > w_v_vint;
-
     SUSYGenEvent susygen;
     SUSYEvent susy;
+    GenEvent gen;
     edm::Wrapper<SUSYGenEvent> w_susygen;
-    edm::Wrapper<SUSYEvent> w_susy; 
+    edm::Wrapper<SUSYEvent> w_susy;
+    edm::Wrapper<GenEvent> w_gen;
   };
 }
