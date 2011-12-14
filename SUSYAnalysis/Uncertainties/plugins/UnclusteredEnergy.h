ce977f5c2b02b8cec7a785fc2bb04b3ae2469da6
npietsch <>
2011-12-14 00:52:07 +0000

    update and code clean up

---
+++ SUSYAnalysis/Uncertainties/plugins/UnclusteredEnergy.h
@@ -0,0 +1,34 @@
+#ifndef UnclusteredEnergy_h
+#define UnclusteredEnergy_h
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+
+class UnclusteredEnergy : public edm::EDProducer {
+
+ public:
+  /// default constructor
+  explicit UnclusteredEnergy(const edm::ParameterSet&);
+  /// default destructor
+  ~UnclusteredEnergy(){};
+  
+ private:
+  /// check settings
+  virtual void beginJob();
+  /// rescale jet energy and recalculated MET
+  virtual void produce(edm::Event&, const edm::EventSetup&);
+
+ private:
+  /// jet input collection 
+  edm::InputTag inputJets_;
+  /// met input collection
+  edm::InputTag inputMETs_;
+  /// scale factor for the rescaling
+  double scaleFactor_;
+  /// jet pt threshold
+  double jetPTThresholdForMET_;
+  /// max jet eta
+  double maxJetEtaForMET_;
+};
+
+#endif
