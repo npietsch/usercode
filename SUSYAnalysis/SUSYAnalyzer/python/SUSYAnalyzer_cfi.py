e94e54c989de78f37bf533db4a553a385196052b
fcostanz <>
2011-12-19 10:03:54 +0000

    New MET region by region Dist Plots added

--- SUSYAnalysis/SUSYAnalyzer/python/SUSYAnalyzer_cfi.py
+++ SUSYAnalysis/SUSYAnalyzer/python/SUSYAnalyzer_cfi.py
@@ -16,6 +16,12 @@ analyzeSUSY = cms.EDAnalyzer("SUSYAnalyzer",
                              RA2weight = cms.InputTag("weightProducer:weight"),
                              TriggerWeight = cms.InputTag("TriggerWeightProducer:triggerWeight"),
                              useEventWeight = cms.bool(False),
-                             useTriggerEventWeight = cms.bool(False)
+                             useTriggerEventWeight = cms.bool(False),
+                             HT0 = cms.double(375),
+                             HT1 = cms.double(650),
+                             HT2 = cms.double(650),   
+                             Y0 = cms.double(3.25),
+                             Y1 = cms.double(5.5),
+                             Y2 = cms.double(5.5),
                              )
                 
