98dfec17767ec804e1fb75a7f53128f4e897875a
npietsch <>
2012-01-24 13:39:32 +0000

    added macro to create PU histogram with different binning

---
+++ SUSYAnalysis/SUSYUtils/data/PU.C
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "TROOT.h"
+#include "TH1D.h"
+#include "TFile.h"
+
+
+void PU() {
+  //TDirectory* keep = gDirectory->GetDirectory("");
+  TFile* ORGPUDIST = new TFile("PU_Run2011.root");
+
+  cout<<"file read"<<endl;
+
+  TH1D* orgpuT = (TH1D*) ORGPUDIST->Get("pileup");//->Clone("puorg");
+  TH1D* orgpu = (TH1D*) orgpuT->Clone("puorg");
+  cout<<"histo cloned"<<endl;
+
+  //delete orgpuT;
+  //keep->cd();
+  //ORGPUDIST->Close();
+
+  TH1D* rebpu = new TH1D("pileup2","pileup", 71, -0.5, 70.5);
+
+  for(int i=1; i<=orgpu->GetXaxis()->GetNbins(); i++) {
+    cout<<i<<"    "<<orgpu->GetBinContent(i)<<endl;
+    rebpu->SetBinContent(i,orgpu->GetBinContent(i));
+    rebpu->SetBinError(i,orgpu->GetBinError(i));
+  }
+  for(int i2=orgpu->GetXaxis()->GetNbins()+1; i2<=rebpu->GetXaxis()->GetNbins(); i2++) {
+    cout<<i2<<"    "<<0<<endl;
+    
+    rebpu->SetBinContent(i2,0);
+  }
+
+  //  orgpu->Delete();
+  //ORGPUDIST->Close();
+
+   rebpu->SetName("pileup");
+
+   rebpu->Draw();
+
+   TFile* REBPUDIST = new TFile("PU_Run2011_bin70.root","RECREATE");
+   rebpu->Write();
+   REBPUDIST->Close();
+   //   ORGPUDIST->Close();
+}
