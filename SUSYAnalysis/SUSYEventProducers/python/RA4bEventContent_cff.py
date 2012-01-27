590ca3349b2c491eec18a914b5dc010bfc1f74ee
npietsch <>
2012-01-27 12:53:42 +0000

    added file to produce sample with smaller event content

---
+++ SUSYAnalysis/SUSYEventProducers/python/RA4bEventContent_cff.py
@@ -0,0 +1,23 @@
+import FWCore.ParameterSet.Config as cms
+
+RA4bEventContent = [
+    'keep *_generator_*_*',
+    'keep *_TriggerResults_*_*',
+    'keep *_offlineBeamSpot_*_*',
+    'keep *_addPileupInfo_*_*',
+    'keep *_genParticles_*_*',
+    'keep *_offlinePrimaryVertices_*_*',
+    'keep *_hltTriggerSummaryAOD_*_*',
+    'keep *_TriggerResults_*_*',
+    'keep *_selectedPatElectrons_*_*',
+    'keep *_selectedPatElectronsPF_*_*',
+    'keep *_selectedPatJetsAK5PF_*_*',
+    'keep *_patMETsPF_*_*',
+    'keep *_patMETsTypeIPF_*_*',
+    'keep *_patMETsTypeIIPF_*_*',
+    'keep *_selectedPatMuons_*_*',
+    'keep *_selectedPatMuonsPF_*_*',
+    'keep *_selectedPatJetsAK5PF_*_*',
+    'keep *_offlinePrimaryVertices_*_*',
+    'keep *_generalTracks_*_*'
+]
