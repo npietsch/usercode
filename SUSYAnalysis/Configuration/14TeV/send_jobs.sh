280aeececf353991f99dc1c2ab514cda88e2c627
npietsch <>
2012-04-21 16:49:02 +0000

    update

--- SUSYAnalysis/Configuration/14TeV/send_jobs.sh
+++ SUSYAnalysis/Configuration/14TeV/send_jobs.sh
@@ -4,11 +4,11 @@ export JOBID=`echo $PBS_JOBID | cut -d. -f1`
 export RUNDIR=/tmp/pbs.${JOBID}
 export VO_CMS_SW_DIR=/afs/naf.desy.de/group/cms/sw
 
-#qsub -l h_vmem=15000M PointA1.sh   ## former A3
+qsub -l h_vmem=20000M PointA1.sh
 
-qsub -l h_vmem=15000M PointB1.sh
+qsub -l h_vmem=20000M PointB1.sh
 
-qsub -l h_vmem=15000M PointC1.sh
+qsub -l h_vmem=20000M PointC1.sh
 
-qsub -l h_vmem=15000M PointD1.sh
+qsub -l h_vmem=20000M PointD1.sh
 
