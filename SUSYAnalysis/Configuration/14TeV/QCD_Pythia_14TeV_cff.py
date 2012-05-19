7288069464d55e1a47fe07a34772db447a309690
npietsch <>
2012-05-19 16:23:46 +0000

    update

---
+++ SUSYAnalysis/Configuration/14TeV/QCD_Pythia_14TeV_cff.py
@@ -0,0 +1,32 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.PythiaUEZ2Settings_cfi import *
+
+
+generator = cms.EDFilter('Pythia6GeneratorFilter',
+	comEnergy = cms.double(14000.0),
+	crossSection = cms.untracked.double(33.80),
+	filterEfficiency = cms.untracked.double(1),
+	maxEventsToPrint = cms.untracked.int32(0),
+	pythiaHepMCVerbosity = cms.untracked.bool(False),
+	pythiaPylistVerbosity = cms.untracked.int32(0),
+
+	PythiaParameters = cms.PSet(
+		pythiaUESettingsBlock,
+		processParameters = cms.vstring(
+			'MSEL = 1        ! QCD hight pT processes',
+			'CKIN(3) = 800   ! minimum pt hat for hard interactions',
+			'CKIN(4) = 1000  ! maximum pt hat for hard interactions',
+		),
+		parameterSets = cms.vstring(
+			'pythiaUESettings',
+			'processParameters',
+		)
+	)
+)
+
+configurationMetadata = cms.untracked.PSet(
+	version = cms.untracked.string('\$Revision: 1.1 $'),
+	name = cms.untracked.string('\$Source: /afs/cern.ch/project/cvs/reps/CMSSW/CMSSW/Configuration/GenProduction/python/FourteenTeV/QCD_Pt_800to1000_TuneZ2star_14TeV_pythia6_cff.py,v $'),
+	annotation = cms.untracked.string('Summer2012-Z2star sample with PYTHIA6: QCD dijet production, pThat = 800 .. 1000 GeV, TuneZ2star')
+)
---
+++ SUSYAnalysis/Configuration/14TeV/Submit_QCD_PAT.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+export JOBID=`echo $PBS_JOBID | cut -d. -f1`
+export RUNDIR=/tmp/pbs.${JOBID}
+export VO_CMS_SW_DIR=/afs/naf.desy.de/group/cms/sw
+
+for i in {1..5}
+do
+cat PAT.sh | sed "s/___JOB___/$i/g" > PAT_"$i".sh
+chmod +x PAT_"$i".sh
+qsub -l h_vmem=10000M -l h_cpu=23:59:00 PAT_"$i".sh
+done
