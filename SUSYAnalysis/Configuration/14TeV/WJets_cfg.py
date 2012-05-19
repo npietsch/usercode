127de8438b92a88d4e36773265dd8748626c135b
npietsch <>
2012-05-19 23:16:11 +0000

    update

--- SUSYAnalysis/Configuration/14TeV/WJets_cfg.py
+++ SUSYAnalysis/Configuration/14TeV/WJets_cfg.py
@@ -1,13 +1,15 @@
 from Ruediger_cfg import *
 
 process.weightProducer.Method = "Constant"
-process.weightProducer.XS = 1.217
-process.weightProducer.NumberEvts = 26898
+process.weightProducer.XS = 1.212 # 1.217 1.211 1.210
+process.weightProducer.NumberEvts = 80270 # 26898 26695 26677
 process.weightProducer.Lumi = 300000  ## Lumi in 1/pb
 
 # Choose input files
 process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring(
-    'file:../../../../../../Storage/ '
+    'file:../../../../../../Storage/WJets_HT700_MET100_14TeV_lvjjj_PAT.root',
+    'file:../../../../../../Storage/WJets_HT700_MET100_14TeV_lvjjj_PAT_2.root',
+    'file:../../../../../../Storage/WJets_HT700_MET100_14TeV_lvjjj_PAT_3.root'
     )
 )
