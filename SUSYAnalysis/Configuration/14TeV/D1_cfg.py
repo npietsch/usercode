d3286769ccba2a79fbaf12d1dadd8ce66de6c479
npietsch <>
2012-05-21 16:24:24 +0000

    update

---
+++ SUSYAnalysis/Configuration/14TeV/D1_cfg.py
@@ -0,0 +1,16 @@
+from Ruediger_cfg import *
+
+process.weightProducer.Method = "Constant"
+process.weightProducer.XS = 0.36
+process.weightProducer.NumberEvts = 100000
+process.weightProducer.Lumi = 300000
+
+# Choose input files
+process.source = cms.Source("PoolSource",
+                            duplicateCheckMode = cms.untracked.string('noDuplicateCheck'),
+                            fileNames = cms.untracked.vstring(
+    'file:../../../../../../Storage/MSSM_14TeV_herwigpp_cff_py_GEN_FASTSIM_HLT_PointD1_PAT_1.root',
+    'file:../../../../../../Storage/MSSM_14TeV_herwigpp_cff_py_GEN_FASTSIM_HLT_PointD1_PAT_2.root'
+    )
+)
+
