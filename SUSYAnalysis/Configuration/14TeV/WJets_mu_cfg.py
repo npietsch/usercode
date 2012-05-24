d17e2d4c141f79121cdc257c7d18baee9248182b
npietsch <>
2012-05-24 08:52:47 +0000

    update

---
+++ SUSYAnalysis/Configuration/14TeV/WJets_mu_cfg.py
@@ -0,0 +1,15 @@
+from Ruediger_cfg import *
+
+process.weightProducer.Method = "Constant"
+process.weightProducer.XS = 1.21 # 1.22 1.20
+process.weightProducer.NumberEvts = 53498 # 26898 26600
+process.weightProducer.Lumi = 300000  ##  Lumi in 1/pb
+
+# Choose input files
+process.source = cms.Source("PoolSource",
+                            duplicateCheckMode = cms.untracked.string('noDuplicateCheck'),
+                            fileNames = cms.untracked.vstring(
+    'file:../../../../../../Storage/WJets_HT700_MET100_14TeV_lvjjj_mu_PAT.root',
+    'file:../../../../../../Storage/WJets_HT700_MET100_14TeV_lvjjj_mu_PAT_2.root'
+    )
+)
