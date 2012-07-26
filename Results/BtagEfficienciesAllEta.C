2dd9631fd64e110024e26cfe18d158482c01b7a9
npietsch <>
2012-07-26 19:40:52 +0000

    update

--- Results/BtagEfficienciesAllEta.C
+++ Results/BtagEfficienciesAllEta.C
@@ -11,7 +11,7 @@
 #include "TStyle.h"
 #include "TLegend.h"
 #include "TPaveText.h"
-#include "TDRStyle_FitBtagEfficienciesAllEta.h"
+#include "TDRStyle_BtagEfficienciesAllEta.h"
 
 vector<TFile*> Files;
 vector<TString> Names;
@@ -61,40 +61,13 @@ void addSelectionStep(TString name, int lc, TString sn)
   SelectionNames.push_back(sn);
 }
 
-// fit ranges
-Double_t x0=40;
-Double_t x1=90;
-Double_t x2=185;
-Double_t x3=670;
-
-// fit function
-Double_t fitf(Double_t *x, Double_t *par)
-{
-  double fitValue=0;
-  
-  if(x0 < x[0] && x[0] <= x1)
-    {
-      fitValue=par[0]+par[1]*(x[0]-x0);
-    }
-  else if(x1 < x[0] && x[0] <= x2) 
-    {
-      fitValue=par[0]+par[1]*(x1-x0);//+par[2]*(x[0]-x1);
-    }
-  else if(x2 <= x[0] && x[0] <= x3) 
-    {
-      fitValue=par[0]+par[1]*(x1-x0)+par[2]*(x[0]-x2);
-    }
-  
-  return fitValue;
- }
-
-int FitBtagEfficienciesAllEta()
+int BtagEfficienciesAllEta()
 {
   //-----------------------------------------------------
   // Define samples
   //-----------------------------------------------------
 
-  TFile* TTJets    = new TFile("TTJetsFall11_2.root", "READ");
+  TFile* TTJets    = new TFile("TTJetsFall11.root", "READ");
   TFile* WJetsHT   = new TFile("WJetsHT.root",        "READ");
   TFile* SingleTop = new TFile("SingleTop.root",      "READ");
 
@@ -107,12 +80,12 @@ int FitBtagEfficienciesAllEta()
   //-----------------------------------------------------
 
   addSample(TTJets,    "T#bar{T}+Jets", kRed,     20, 1.1, 7);
-//   addSample(SingleTop, "single top",    kBlue,    20, 1.1, 7);
-//   addSample(WJetsHT,   "W+Jets",        kGreen+2, 23, 1.2, 7);
+  addSample(SingleTop, "single top",    kBlue,    21, 0.9, 7);
+  addSample(WJetsHT,   "W+Jets",        kGreen+2, 22, 1.2, 7);
 
-//   addSample(LM3,  "LM3",  kRed+2,   20, 0.9, 7);
-//   addSample(LM8,  "LM8",  kBlue,    23, 1.1, 7);
-//   addSample(LM13, "LM13", kGreen+2, 24, 1.2, 7);
+//   addSample(LM3,  "LM3",  kRed,     20, 1.1, 7);
+//   addSample(LM8,  "LM8",  kBlue,    21, 0.9, 7);
+//   addSample(LM13, "LM13", kGreen+2, 22, 1.2, 7);
 
   //-----------------------------------------------------
   // addAlgorithm(TString name)
@@ -125,8 +98,8 @@ int FitBtagEfficienciesAllEta()
   //-----------------------------------------------------
 
   Flavors.push_back("B");
-  //Flavors.push_back("C");
-  //Flavors.push_back("L");
+  Flavors.push_back("C");
+  Flavors.push_back("L");
 
   //-----------------------------------------------------
   // addSelectionStep(TString name, int lc, TString sn);
@@ -156,13 +129,12 @@ int FitBtagEfficienciesAllEta()
 	      // Define canvas, legend and labels
 	      TCanvas *canvas =new TCanvas(SelectionNames[s]+"_"+Algos[a]+"_"+Flavors[flv]+"_Pt",SelectionNames[s]+"_"+Algos[a]+"_"+Flavors[flv]+"_Pt",1);
 
-	      TLegend *leg = new TLegend(.68,.18,.91,.245);
-	      //TLegend *leg = new TLegend(.64,.18,.91,.35);
+	      TLegend *leg = new TLegend(.64,.18,.91,.35);
 	      leg->SetTextFont(42);
 	      leg->SetFillColor(0);
 	      leg->SetLineColor(1);
 	      
-	      TPaveText *label = new TPaveText(0.14,0.91,0.6,0.99,"NDC");
+	      TPaveText *label = new TPaveText(0.16,0.85,0.62,0.93,"NDC");
 	      label->SetFillColor(0);
 	      label->SetTextFont(42);
 	      label->SetBorderSize(0);
@@ -175,15 +147,6 @@ int FitBtagEfficienciesAllEta()
 	      label2->SetBorderSize(0);
 	      TText *text2=label2->AddText("0 < |#eta| < 2.4");
 	      text2->SetTextAlign(22);
-
-	      TPaveText *label3 = new TPaveText(.60,.68,.91,.85,"NDC");
-	      label3->SetFillColor(0);
-	      label3->SetTextFont(42);
-	      label3->SetBorderSize(1);
-	      TText *text3=label3->AddText("p0    0.63195 #pm 0.00266");
-	      TText *text4=label3->AddText("p1    0.00216 #pm 0.00007");
-	      TText *text5=label3->AddText("p2   -0.00054 #pm 0.00003");
-	      text3->SetTextAlign(22);
 
 	      // declare Maximum and ybin
 	      double Maximum=0;
@@ -202,9 +165,9 @@ int FitBtagEfficienciesAllEta()
 		  TaggedPt_->Divide(Pt_);
 		  
 		  // define shifts of markers in x direction
-		  double shift_=3*(f);
-		  double shift2_=5*(f);
-		  double shift3_=7*(f);
+		  double shift_=3*(f-1);
+		  double shift2_=5*(f-1);
+		  double shift3_=7*(f-1);
 		  
 		  // define int nBins and arrays xbinsPt and xbinsPtX
 		  Int_t nBins=TaggedPt_->GetNbinsX();
@@ -255,105 +218,61 @@ int FitBtagEfficienciesAllEta()
 		  // Define and draw histograms
 		  //--------------------------------------
 
-		  // Draw marker
-		  Tmp_->SetTitle("");
+		  // Draw x-errors
+		  Tmp2_->SetTitle("");
 
 		  if(Flavors[flv]=="B")
 		    {
-		      Tmp_->SetMaximum(1);
-		      Tmp_->SetMinimum(0);
-		      Tmp_->GetXaxis()->SetTitle("b-jet p_{T} [GeV]");
-		      Tmp_->GetYaxis()->SetTitle("b-tag efficiency");
+		      Tmp2_->SetMaximum(1.05*0.861736);
+		      Tmp2_->SetMinimum(0);
+		      Tmp2_->GetXaxis()->SetTitle("b-jet p_{T} [GeV]");
+		      Tmp2_->GetYaxis()->SetTitle("b-tag efficiency");
 		    }
 		  if(Flavors[flv]=="C")
 		    {
-		      Tmp_->SetMaximum(1.05*0.599472);
-		      Tmp_->SetMinimum(0);
-		      Tmp_->GetXaxis()->SetTitle("c-jet p_{T} [GeV]");
-		      Tmp_->GetYaxis()->SetTitle("mistag efficiency");
+		      Tmp2_->SetMaximum(1.05*0.34543);
+		      Tmp2_->SetMinimum(0);
+		      Tmp2_->GetXaxis()->SetTitle("c-jet p_{T} [GeV]");
+		      Tmp2_->GetYaxis()->SetTitle("mistag efficiency");
 		    }
 		  if(Flavors[flv]=="L")
 		    {
-		      Tmp_->SetMaximum(1.05*0.117256);
-		      Tmp_->SetMinimum(0);
-		      Tmp_->GetXaxis()->SetTitle("light quark/gluon jet p_{T} [GeV]");
-		      Tmp_->GetYaxis()->SetTitle("mistag efficiency");
+		      Tmp2_->SetMaximum(1.05*0.0972257);
+		      Tmp2_->SetMinimum(0);
+		      Tmp2_->GetXaxis()->SetTitle("light quark/gluon jet p_{T} [GeV]");
+		      Tmp2_->GetYaxis()->SetTitle("mistag efficiency");
 		    }
 
-		  Tmp_->GetXaxis()->SetTitleOffset(1.2); 
-		  Tmp_->GetYaxis()->SetTitleOffset(1.);
+		  Tmp2_->GetXaxis()->SetTitleOffset(1.2); 
+		  Tmp2_->GetYaxis()->SetTitleOffset(1.3);
+		  
+		  Tmp2_->SetLineColor(SampleColors[f]);
+		  Tmp2_->SetLineWidth(1);
+		  if(f==0) Tmp2_->Draw("");
+		  else Tmp2_->Draw("same");
+		  
+		  // Draw markers
 		  Tmp_->SetLineColor(SampleColors[f]);
 		  Tmp_->SetLineWidth(1);
 		  Tmp_->SetMarkerStyle(MarkerStyles[f]);
 		  Tmp_->SetMarkerColor(SampleColors[f]);
-		  Tmp_->SetMarkerSize(MarkerSizes[f]);		      
-		  if(f==0) Tmp_->Draw("E x0");
-		  else Tmp_->Draw("same E x0");
+		  Tmp_->SetMarkerSize(MarkerSizes[f]);
+		  Tmp_->Draw("same E x0");
 		  
-		  // Draw x-errors
-		  Tmp2_->SetLineColor(SampleColors[f]);
-		  Tmp2_->SetLineWidth(1);
-		  Tmp2_->Draw("same");
-
-		  // fit function
-		  Double_t parameters[6];
-
- 		  if(Flavors[flv] == "B")
- 		    {
-		      TF1 *g1 = new TF1("g1","[0]+[1]*x",x0,x1);
-		      g1->SetLineColor(SampleColors[f]);
-		      g1->SetLineWidth(1);
-		      Tmp_->Fit(g1,"R");
-		      Tmp_->GetFunction("g1")->ResetBit(1<<9);
-		      
-		      TF1 *g2 = new TF1("g2","[0]+[1]*x",x1,x2);
-		      g2->SetLineColor(SampleColors[f]);
-		      g2->SetLineWidth(1);
-		      Tmp_->Fit(g2,"R+");
-		      Tmp_->GetFunction("g2")->ResetBit(1<<9);
-
-		      TF1 *g3 = new TF1("g3","[0]+[1]*x",x2,x3);
-		      g3->SetLineColor(SampleColors[f]);
-		      g3->SetLineWidth(1);
-		      Tmp_->Fit(g3,"R+");
-		      Tmp_->GetFunction("g3")->ResetBit(1<<9);
-
-		      g1->GetParameters(&parameters[0]);
-		      g2->GetParameters(&parameters[2]);
-		      g3->GetParameters(&parameters[4]);
-
-		      TF1 *func = new TF1("fitf", fitf, x0, x3, 3);
-
-		      // Sets initial values
-		      func->SetParameters(parameters[0],parameters[1],parameters[5]);
-		      //func->SetParameters(6.32386e-01, 2.13442e-03, 2.56885e-05, -5.44586e-04);
-		      func->SetLineColor(1);
-		      func->SetLineWidth(3);
-		      //func->SetNpx(1000);
-		      //func->Draw("same");
-
-		      // Fit histogram in range defined by function
-		      Tmp_->Fit(func,"R");
-		      //Tmp_->GetFunction("func")->ResetBit(1<<9);
-
-		      func->SetParameters(0,0.1);
-
-		    }
 		  leg->AddEntry(Tmp_,Names[f],"l P");
-
+		}
+	      
 	      std::cout << "==============================" << std::endl;
 	      std::cout << "Maximum: " << Maximum << std::endl;
 	      std::cout << "==============================" << std::endl;
-
+	      
 	      leg->SetShadowColor(0);
-	      //leg->Draw();
+	      leg->Draw();
 	      label->Draw();
 	      //label2->Draw();
-	      label3->Draw();
-
-	      canvas->SaveAs(Algos[a]+"_"+Flavors[flv]+"jetsEfficiency.pdf");
+	      //label3->Draw();
 	      
-		}
+	      canvas->SaveAs(Algos[a]+"_"+Flavors[flv]+"jetsEfficiency_SM.pdf");
 	    }
 	}
     }
