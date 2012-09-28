9954496f98dd4ac14973181b037199fe9a4bde71
mstein <>
2012-09-28 09:39:29 +0000

    played a bit with the analyser

--- SUSYAnalysis/SUSYAnalyzer/plugins/TestAnalyzer.cc
+++ SUSYAnalysis/SUSYAnalyzer/plugins/TestAnalyzer.cc
@@ -1,20 +1,37 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "SUSYAnalysis/SUSYAnalyzer/plugins/TestAnalyzer.h"
 
-using namespace std;
- 
-TestAnalyzer::TestAnalyzer(const edm::ParameterSet& cfg):
-  muons_        (cfg.getParameter<edm::InputTag>("muons")),
-  electrons_    (cfg.getParameter<edm::InputTag>("electrons")),
-  jets_         (cfg.getParameter<edm::InputTag>("jets"))
 
-  
+using namespace std;
+
+TestAnalyzer::TestAnalyzer(const edm::ParameterSet& cfg):
+muons_        (cfg.getParameter<edm::InputTag>("muons")),
+electrons_    (cfg.getParameter<edm::InputTag>("electrons")),
+jets_         (cfg.getParameter<edm::InputTag>("jets"))
 { 
   edm::Service<TFileService> fs;
+  JetsEt_     = fs->make<TH1F>("JetsEt" ,"JetsEt",  90, 0., 900.);
+  JetsEta_    = fs->make<TH1F>("JetsEta","JetsEta",    60, -3, 3);
+  nJets_      = fs->make<TH1F>("nJets"  ,"nJets",   16, -0.5, 15.5);
+  nelectrons_ = fs->make<TH1F>("nElectrons"  ,"nElectrons",   16, -0.5, 15.5);
+  nMuons_     = fs->make<TH1F>("nMuons"  ,"nMuons",   16, -0.5, 15.5);
+  nLeptons_   = fs->make<TH1F>("nLeptons"  ,"nLeptons",   16, -0.5, 15.5);
+  MuMuMass_   = fs->make<TH1F>("MuMuMass_" ,"MuMuMass_",  200, 0., 500.);
 
-  JetsEt_ = fs->make<TH1F>("JetsEt" ,"JetsEt",  90, 0., 900.);
-  JetsEta_= fs->make<TH1F>("JetsEta","JetsEta",    60, -3, 3);
-  nJets_  = fs->make<TH1F>("nJets"  ,"nJets",   16, -0.5, 15);
+
+
+  myTree = 0;
+  myTree = new TTree("myTree","myTree");
+
+  myTree->Branch("diMuMass", &diMuMass, "diMuMass/D");
+  myTree->Branch("nLeptons", &nLeptons, "nLeptons/I");
+  myTree->Branch("goodMuon", &goodMuon);
+  
+
+
+
+
+
 }
 
 TestAnalyzer::~TestAnalyzer()
@@ -23,6 +40,8 @@ TestAnalyzer::~TestAnalyzer()
 
 void
 TestAnalyzer::analyze(const edm::Event& evt, const edm::EventSetup& setup){
+  diMuMass = 0.;
+  nLeptons = 0;
 
   //-------------------------------------------------
   // Handles
@@ -30,33 +49,69 @@ TestAnalyzer::analyze(const edm::Event& evt, const edm::EventSetup& setup){
   
   edm::Handle<std::vector<pat::Jet> > jets;
   evt.getByLabel(jets_, jets);
-
+  
   edm::Handle<std::vector<pat::Muon> > muons;
   evt.getByLabel(muons_, muons);
-
+  
   edm::Handle<std::vector<pat::Electron> > electrons;
   evt.getByLabel(electrons_, electrons);
-
+  
   //-------------------------------------------------
   // event weight
   //-------------------------------------------------
-
+  
   double weight=1;
-
+  
   //-------------------------------------------------
   // Basic kinematics
   //-------------------------------------------------
-
+  
   // Jets
-  for(int idx=0; idx<(int)jets->size(); ++idx)
-    {
-      JetsEt_->Fill((*jets)[idx].et(),weight);
-      JetsEta_->Fill((*jets)[idx].eta());
-    }
-	
+  for(int idx=0; idx<(int)jets->size(); ++idx){
+    JetsEt_->Fill((*jets)[idx].et(),weight);
+    JetsEta_->Fill((*jets)[idx].eta(),weight);
+  }
   nJets_->Fill(jets->size(),weight);
+  
+  nelectrons_->Fill(electrons->size(),weight);
+  nMuons_->Fill(muons->size(),weight);
+  nLeptons_->Fill(muons->size() + electrons->size(),weight);
+  
+  for(Int_t i=0,N=muons->size();i<N-1; ++i){
+    for(Int_t j=i+1; j<N; ++j){
+      //if(muons[i]->charge==muons[j]->charge()) continue;
+      if((*muons)[i].charge()==(*muons)[j].charge()) continue;
+      double mass = ((*muons)[i].p4() + (*muons)[j].p4()).mass();
+      MuMuMass_->Fill(mass, weight);
+      diMuMass=mass;
+      //cout<<"mass = " << mass << endl;
+      
+
+    }
+
+    //LorentzV dummy((*muons)[i].p4());
+    //math::XYZTLorentzVector dummy((*muons)[i].p4());
+    //goodMuon.push_back(dummy);
+    goodMuon.push_back((*muons)[i].p4());
+
+  }
+  nLeptons = muons->size() + electrons->size();
+  
+
+
+
+
+
+
+
+
+  myTree->Fill();
+
 
 }
+
+
+
 
 void TestAnalyzer::beginJob()
 {  
@@ -64,6 +119,9 @@ void TestAnalyzer::beginJob()
 
 void TestAnalyzer::endJob()
 {
+  myTree->Write();
+
 }
 
 DEFINE_FWK_MODULE(TestAnalyzer);
+
--- SUSYAnalysis/SUSYAnalyzer/plugins/TestAnalyzer.h
+++ SUSYAnalysis/SUSYAnalyzer/plugins/TestAnalyzer.h
@@ -3,6 +3,10 @@
 
 #include "TH1.h"
 #include "TH2.h"
+#include "TTree.h"
+#include <vector>
+#include <Math/LorentzVector.h>
+#include <math.h>
 
 #include "FWCore/Framework/interface/EDAnalyzer.h"
 #include "FWCore/Framework/interface/Event.h"
@@ -15,30 +19,58 @@
 #include "DataFormats/PatCandidates/interface/Muon.h"
 #include "DataFormats/PatCandidates/interface/Electron.h"
 
+//#ifdef __MAKECINT__
+//#pragma link C++ class ROOT::Math::PtEtaPhiM4D<float>+;
+//#endif
+
+//typedef ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<float> > LorentzV;
+
+
+using namespace std;
+
+
+
 class TestAnalyzer : public edm::EDAnalyzer {
-
- public:
   
-  explicit TestAnalyzer(const edm::ParameterSet&);
-  ~TestAnalyzer();
+  public:
+    
+    explicit TestAnalyzer(const edm::ParameterSet&);
+    ~TestAnalyzer();
+    
+  private:
+    
+    virtual void beginJob() ;
+    virtual void analyze(const edm::Event&, const edm::EventSetup&);
+    virtual void endJob();
+    
+    // Input tags
+    edm::InputTag muons_;
+    edm::InputTag electrons_;
+    edm::InputTag jets_;
+    
+    // Histograms
+    TH1F* JetsEt_;
+    TH1F* JetsEta_;
+    TH1F* nJets_;
+    TH1F* nelectrons_;
+    TH1F* nMuons_;
+    TH1F* nLeptons_;
+    
+    TH1F* MuMuMass_;
 
- private:
+    TTree* myTree;
 
-  virtual void beginJob() ;
-  virtual void analyze(const edm::Event&, const edm::EventSetup&);
-  virtual void endJob();
+    double diMuMass;
+    int nLeptons;
+    
+    //std::vector<LorentzV> *goodMuon; 
+    //vector<LorentzVector> goodMuon;
+    vector<math::XYZTLorentzVector> goodMuon;
 
-  // Input tags
-  edm::InputTag muons_;
-  edm::InputTag electrons_;
-  edm::InputTag jets_;
 
-  // Histograms
-  TH1F* JetsEt_;
-  TH1F* JetsEta_;
-  TH1F* nJets_;
 
-};  
+};
+
 
 #endif  
 
