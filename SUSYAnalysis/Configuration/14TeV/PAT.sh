6c0f07aa1445b70d6b86ab8e7e5824396c9c4aee
npietsch <>
2012-10-24 10:32:00 +0000

    update

--- SUSYAnalysis/Configuration/14TeV/PAT.sh
+++ SUSYAnalysis/Configuration/14TeV/PAT.sh
@@ -8,14 +8,14 @@ export RUNDIR=/tmp/pbs.${JOBID}
 export VO_CMS_SW_DIR=/afs/naf.desy.de/group/cms/sw
 source ${VO_CMS_SW_DIR}/cmsset_default.sh
 
-export CMSSWDIR=/scratch/hh/lustre/cms/user/npietsch/Analysis/CMSSW_4_4_4
-export FILEDIR=/scratch/hh/lustre/cms/user/npietsch/Analysis/CMSSW_4_4_4/src/SUSYAnalysis/Configuration/14TeV
-export WORKDIR=/scratch/hh/lustre/cms/user/npietsch/Analysis/CMSSW_4_4_4/src/SUSYAnalysis/Configuration/14TeV/naf_QCD____JOB___
+export CMSSWDIR=/scratch/hh/lustre/cms/user/npietsch/QCD/CMSSW_4_4_4
+export FILEDIR=/scratch/hh/lustre/cms/user/npietsch/QCD/CMSSW_4_4_4/src/SUSYAnalysis/Configuration/14TeV
+export WORKDIR=/scratch/hh/lustre/cms/user/npietsch/QCD/CMSSW_4_4_4/src/SUSYAnalysis/Configuration/14TeV/naf_QCD____JOB___
 
 cd ${WORKDIR}
 cd ${CMSSWDIR}/src
 cmsenv
 cd -
 
-cp ${FILEDIR}/SUSY_pattuple_cfg.py ./
-cmsRun -p SUSY_pattuple_cfg.py &> log.txt
+cp ${FILEDIR}/SUSY_pattuple_QCD_cfg.py ./
+cmsRun -p SUSY_pattuple_QCD_cfg.py &> log.txt
