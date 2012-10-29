80de6177bdb45cc6366e5e92e9a790bce6504e5c
npietsch <>
2012-10-29 14:07:24 +0000

    update

--- SUSYAnalysis/Configuration/14TeV/QCD_1_cfg.py
+++ SUSYAnalysis/Configuration/14TeV/QCD_1_cfg.py
@@ -2,7 +2,7 @@ from Ruediger_cfg import *
 
 process.weightProducer.Method = "PtHat"
 process.weightProducer.XS = 10.62E+10 #2317000000
-process.weightProducer.NumberEvts = 19850000
+process.weightProducer.NumberEvts = 38550000
 process.weightProducer.Lumi = 300000  ## Lumi in 1/pb
 
 # Choose input files
--- SUSYAnalysis/Configuration/14TeV/Submit_jobs.sh
+++ SUSYAnalysis/Configuration/14TeV/Submit_jobs.sh
@@ -13,4 +13,6 @@ nafJobSplitter.pl -q 1 3 WJets_cfg.py
 nafJobSplitter.pl -q 1 2 WJets_mu_cfg.py
 
 nafJobSplitter.pl -q 1 197 QCD_1_cfg.py
-nafJobSplitter.pl -q 1 200 QCD_1_cfg.py
+nafJobSplitter.pl -q 1 200 QCD_2_cfg.py
+nafJobSplitter.pl -q 1 200 QCD_3_cfg.py
+nafJobSplitter.pl -q 1 174 QCD_4_cfg.py
