2a04f1d2a63ad56065f437fec34ecf6bf17b45ad
npietsch <>
2012-10-29 15:18:29 +0000

    update

--- SUSYAnalysis/Configuration/14TeV/WJets_mu_cfg.py
+++ SUSYAnalysis/Configuration/14TeV/WJets_mu_cfg.py
@@ -1,8 +1,8 @@
 from Ruediger_cfg import *
 
 process.weightProducer.Method = "Constant"
-process.weightProducer.XS = 1.21 # 1.22 1.20
-process.weightProducer.NumberEvts = 53498 # 26898 26600
+process.weightProducer.XS = 1.21 # 1.22 1.20 1.22
+process.weightProducer.NumberEvts = 80498 # 26898 26600 27000
 process.weightProducer.Lumi = 300000  ##  Lumi in 1/pb
 
 # Choose input files
