05c8f0cd8aa1fa4a3162f405b9075beec65be641
npietsch <>
2012-11-08 18:08:06 +0000

    update

--- SUSYAnalysis/Configuration/14TeV/SemiLepTTJets_cfg.py
+++ SUSYAnalysis/Configuration/14TeV/SemiLepTTJets_cfg.py
@@ -1,15 +1,16 @@
 from Ruediger_cfg import *
 
 process.weightProducer.Method = "Constant"
-process.weightProducer.XS = 0.4363
-process.weightProducer.NumberEvts = 89997
+process.weightProducer.XS = 0.4366 ## 0.4363 4.370
+process.weightProducer.NumberEvts = 179991 ## 89997 89994
 process.weightProducer.Lumi = 300000 ## Lumi in 1/pb
 
 # Choose input files
 process.source = cms.Source("PoolSource",
                             duplicateCheckMode = cms.untracked.string('noDuplicateCheck'),
                             fileNames = cms.untracked.vstring(
-    'file:../../../../../../BackgroundSamples/SemiLepTTJetsNew_HT700_MET100_14TeV_PAT_1.root'
+    'file:../../../../../../BackgroundSamples/SemiLepTTJetsNew_HT700_MET100_14TeV_PAT_1.root',
+    'file:../../../../../../BackgroundSamples/SemiLepTTJetsNew_HT700_MET100_14TeV_PAT_2.root'
     )
 )
 
