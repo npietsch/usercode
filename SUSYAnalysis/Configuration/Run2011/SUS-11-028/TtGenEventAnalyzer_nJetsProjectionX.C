58b0527fd731d806df39d8a8a91bef7efd24eee4
npietsch <>
2012-12-17 21:08:15 +0000

    update

--- SUSYAnalysis/Configuration/Run2011/SUS-11-028/TtGenEventAnalyzer_nJetsProjectionX.C
+++ SUSYAnalysis/Configuration/Run2011/SUS-11-028/TtGenEventAnalyzer_nJetsProjectionX.C
@@ -13,19 +13,35 @@
 #include "TPaveText.h"
 #include <TDRStyle.h>
 
+// Samples
+vector<TFile*> Files;
+vector<TString> Labels;
+vector<TString> Names;
+
+// Histograms
 vector<TString> Histograms;
 vector<TString> XLabels;
 vector<unsigned int> FirstValues;
 vector<unsigned int> LastValues;
 
+// Selections
 vector<TString> Selections;
 vector<TString> SelectionLabels;
 
+// Bins
 vector<unsigned int> FirstBins;
 vector<unsigned int> LastBins;
 vector<TString> BinLabels;
 vector<unsigned int> BinColors;
 vector<unsigned int> MarkerStyles;
+
+// add sample
+void addSample(TFile* sample, TString label, TString name)
+{
+  Files.push_back(sample);
+  Labels.push_back(label);
+  Names.push_back(name);
+}
 
 // add histogram
 void addHistogram(TString name, TString xLabel, int firstValue, int lastValue)
@@ -57,17 +73,25 @@ void addBin(int firstBin, int lastBin, TString binLabel, int binColor, int marke
 int TtGenEventAnalyzer_nJetsProjectionX()
 {
 
-  //--------------------------------------------------------------
+  //---------------------------------------------------------------------------------
   // Sample
-  //--------------------------------------------------------------
+  //---------------------------------------------------------------------------------
 
   //TFile* TTJets = new TFile("SemiLepElMu.root", "READ");
-  TFile* TTJets = new TFile("SingleTop.root", "READ");
-  // TFile* TTJets = new TFile("DiLep.root", "READ");
+  //TFile* TTJets = new TFile("DiLep.root", "READ");
+  TFile* SingleTop = new TFile("SingleTop.root", "READ");
+  TFile* WJetsHT   = new TFile("WJetsHT.root", "READ");
 
-  //--------------------------------------------------------------
-  // addHistogram
-  //--------------------------------------------------------------
+  //---------------------------------------------------------------------------------
+  // addSample(TFile* sample, TString label)
+  //---------------------------------------------------------------------------------
+
+  addSample(SingleTop, "Single Top", "SingleTop");
+  addSample(WJetsHT,   "W+Jets",     "WJets");
+
+  //---------------------------------------------------------------------------------
+  // addHistogram(TString name, TString xLabel, int firstValue, int lastValue)
+  //---------------------------------------------------------------------------------
 
   //addHistogram("YMET_nJets",    "YMET [GeV^{1/2}]", 0, 25);
   addHistogram("mT_nJets",      "m_{T} [GeV]",      0, 300);
@@ -75,10 +99,10 @@ int TtGenEventAnalyzer_nJetsProjectionX()
   //addHistogram("mlb_nJets",     "mlb [GeV]",        0, 300);
   //addHistogram("mLepTop_nJets", "mLepTop [GeV]",    0, 400);
 
-  //--------------------------------------------------------------
-  // addSelectionStep
-  //--------------------------------------------------------------
-  
+  //---------------------------------------------------------------------------------
+  // addSelectionStep(TString step, TString selectionLabel)
+  //---------------------------------------------------------------------------------
+
   addSelectionStep("analyzeCorrelation1l", "lepton selection");
 
 //   addSelectionStep("analyzeCorrelation1l_YMET0To1", "lepton selection");
@@ -88,9 +112,9 @@ int TtGenEventAnalyzer_nJetsProjectionX()
 //   addSelectionStep("analyzeCorrelation1l_YMET4To5", "lepton selection");
 //   addSelectionStep("analyzeCorrelation1l_YMET5To6", "lepton selection");
 
-  //--------------------------------------------------------------
-  // addBin
-  //--------------------------------------------------------------
+  //---------------------------------------------------------------------------------
+  // addBin(int firstBin, int lastBin, TString binLabel, int binColor, int marker)
+  //---------------------------------------------------------------------------------
 
   //addBin(4, 5,  "3-4 Jets", 2, 22);
   //addBin(6, 7,  "5-6 Jets", 4, 23);
@@ -106,89 +130,94 @@ int TtGenEventAnalyzer_nJetsProjectionX()
 
   setTDRStyle();
 
-  for(int hdx=0; hdx<(int)Histograms.size(); ++hdx)
-    { 
-      std::cout << "\n" << Histograms[hdx] << std::endl;
-      std::cout << "-----------------------" << std::endl;
-
-      for(int sdx=0; sdx<(int)Selections.size(); ++sdx)
-	{
-	  std::cout << Selections[sdx] << "_" << Histograms[hdx] << std::endl;
-	  
-	  TCanvas *canvas = new TCanvas(Selections[sdx]+"_"+Histograms[hdx],Selections[sdx]+"_"+Histograms[hdx], 1);
-
-	  // legend
-	  TLegend *leg = new TLegend(.56,.67,.90,.91);
-	  leg->SetTextFont(42);
-	  leg->SetTextSize(0.055);
-	  leg->SetFillColor(0);
-	  leg->SetLineColor(1);
-	  leg->SetShadowColor(0);
-	  leg->SetLineColor(0);
-	  leg->AddEntry((TObject*)0, "Semilep. t#bar{t}", "");
-
-	  // label
-	  TPaveText *label = new TPaveText(0.14,0.94,0.99,1.,"NDC");
-	  label->SetFillColor(0);
-	  label->SetTextFont(42);
-	  label->SetTextSize(0.043);
-	  label->SetBorderSize(0);
-	  label->SetTextAlign(12);
-	  TText *text=label->AddText("Simulation, #sqrt{s} = 7 TeV");
+  for(int fdx=0; fdx<(int)Files.size(); ++fdx)
+    {
+      std::cout << "\n" << Files[fdx] << std::endl;
       
-	  TH2F* Hist = (TH2F*)TTJets->Get(Selections[sdx]+"/"+Histograms[hdx]);
-
-	  for(int bin=0; bin<(int)FirstBins.size(); ++bin)
-	    {
-	      std::cout << "bins " << FirstBins[bin] << " - " << LastBins[bin] << std::endl;
-
-	      // create projection
-	      TH1F* Projection = (TH1F*)Hist->ProjectionX(Histograms[hdx], FirstBins[bin], LastBins[bin],"");
-	      
-	      // titles, scales and ranges
-	      Projection->SetTitle("");
-
-	      Projection->GetXaxis()->SetTitle(XLabels[hdx]);
-	      Projection->GetXaxis()->SetTitleSize(0.05);
-	      Projection->GetYaxis()->SetTitleOffset(1.4);
-	      Projection->GetXaxis()->SetRangeUser(FirstValues[hdx],LastValues[hdx]);
-	      
-	      Projection->GetYaxis()->SetTitle("a.u.");
-	      Projection->GetYaxis()->SetTitleSize(0.05);
-	      Projection->GetYaxis()->SetTitleSize(0.05);
-
-	      Projection->Scale(1/Projection->Integral(1,-1));
-
-	      // lines
-	      Projection->SetLineColor(BinColors[bin]);
-	      Projection->SetLineWidth(2);
-
-	      // markers
-	      Projection->SetMarkerStyle(MarkerStyles[bin]);
-	      Projection->SetMarkerColor(BinColors[bin]);
-
-	      // add entry to legend
-	      leg->AddEntry(Projection->Clone(),BinLabels[bin],"l P");
-
-	      if(bin == 0) Projection->DrawCopy();
-	      else Projection->DrawCopy("same");
-	    }
-
-	  TPaveText *label2 = new TPaveText(0.45,0.38,0.75,0.54,"NDC");
-	  label2->SetFillColor(0);
-	  label2->SetTextFont(62);
-	  label2->SetTextSize(0.07);
-	  label2->SetBorderSize(0);
-	  label2->SetTextAlign(12);
-	  TText *text2=label2->AddText("Own work");
-	  TText *text3=label2->AddText("in progress");
+      for(int hdx=0; hdx<(int)Histograms.size(); ++hdx)
+	{ 
+	  std::cout << "\n" << Histograms[hdx] << std::endl;
+	  std::cout << "-----------------------" << std::endl;
 	  
-	  label->Draw("same");
-	  //label2->Draw("same");
-	  leg->Draw();
+	  for(int sdx=0; sdx<(int)Selections.size(); ++sdx)
+	    {
+	      std::cout << Selections[sdx] << "_" << Histograms[hdx] << std::endl;
+	      
+	      TCanvas *canvas = new TCanvas(Selections[sdx]+"_"+Histograms[hdx]+"_"+Labels[fdx],Selections[sdx]+"_"+Histograms[hdx]+"_"+Labels[fdx], 1);
+	      
+	      // legend
+	      TLegend *leg = new TLegend(.56,.67,.90,.91);
+	      leg->SetTextFont(42);
+	      leg->SetTextSize(0.055);
+	      leg->SetFillColor(0);
+	      leg->SetLineColor(1);
+	      leg->SetShadowColor(0);
+	      leg->SetLineColor(0);
+	      leg->AddEntry((TObject*)0, Labels[fdx], "");
+	      
+	      // label
+	      TPaveText *label = new TPaveText(0.14,0.94,0.99,1.,"NDC");
+	      label->SetFillColor(0);
+	      label->SetTextFont(42);
+	      label->SetTextSize(0.043);
+	      label->SetBorderSize(0);
+	      label->SetTextAlign(12);
+	      TText *text=label->AddText("Simulation, #sqrt{s} = 7 TeV");
+	      
+	      TH2F* Hist = (TH2F*)Files[fdx]->Get(Selections[sdx]+"/"+Histograms[hdx]);
+	      
+	      for(int bin=0; bin<(int)FirstBins.size(); ++bin)
+		{
+		  std::cout << "bins " << FirstBins[bin] << " - " << LastBins[bin] << std::endl;
+		  
+		  // create projection
+		  TH1F* Projection = (TH1F*)Hist->ProjectionX(Histograms[hdx], FirstBins[bin], LastBins[bin],"");
+		  
+		  // titles, scales and ranges
+		  Projection->SetTitle("");
+		  
+		  Projection->GetXaxis()->SetTitle(XLabels[hdx]);
+		  Projection->GetXaxis()->SetTitleSize(0.05);
+		  Projection->GetYaxis()->SetTitleOffset(1.4);
+		  Projection->GetXaxis()->SetRangeUser(FirstValues[hdx],LastValues[hdx]);
+		  
+		  Projection->GetYaxis()->SetTitle("a.u.");
+		  Projection->GetYaxis()->SetTitleSize(0.05);
+		  Projection->GetYaxis()->SetTitleSize(0.05);
+		  
+		  Projection->Scale(1/Projection->Integral(1,-1));
 
-	  canvas->SetLogy();
-	  canvas->SaveAs(Selections[sdx]+"_"+Histograms[hdx]+"_ProjectionX_log.pdf");
+		  // lines
+		  Projection->SetLineColor(BinColors[bin]);
+		  Projection->SetLineWidth(2);
+	      
+		  // markers
+		  Projection->SetMarkerStyle(MarkerStyles[bin]);
+		  Projection->SetMarkerColor(BinColors[bin]);
+		  
+		  // add entry to legend
+		  leg->AddEntry(Projection->Clone(),BinLabels[bin],"l P");
+		  
+		  if(bin == 0) Projection->DrawCopy();
+		  else Projection->DrawCopy("same");
+		}
+	      
+	      TPaveText *label2 = new TPaveText(0.45,0.38,0.75,0.54,"NDC");
+	      label2->SetFillColor(0);
+	      label2->SetTextFont(62);
+	      label2->SetTextSize(0.07);
+	      label2->SetBorderSize(0);
+	      label2->SetTextAlign(12);
+	      TText *text2=label2->AddText("Own work");
+	      TText *text3=label2->AddText("in progress");
+	      
+	      label->Draw("same");
+	      //label2->Draw("same");
+	      leg->Draw();
+	      
+	      canvas->SetLogy();
+	      canvas->SaveAs(Selections[sdx]+"_"+Histograms[hdx]+"_"+Names[fdx]+"_ProjectionX_log.pdf");
+	    }
 	}
     }	  
 }
