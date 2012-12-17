d7e9251e5798e40e0a37e59ab4883d31a9fde371
npietsch <>
2012-12-17 20:36:12 +0000

    update

--- SUSYAnalysis/Configuration/Run2011/SUS-11-028/TtGenEventAnalyzer_nJetsProjectionX.C
+++ SUSYAnalysis/Configuration/Run2011/SUS-11-028/TtGenEventAnalyzer_nJetsProjectionX.C
@@ -11,7 +11,7 @@
 #include "TStyle.h"
 #include "TLegend.h"
 #include "TPaveText.h"
-#include <TDRStyle_TtGenEventAnalyzer.h>
+#include <TDRStyle.h>
 
 vector<TString> Histograms;
 vector<TString> XLabels;
@@ -28,8 +28,6 @@ vector<unsigned int> BinColors;
 vector<unsigned int> MarkerStyles;
 
 // add histogram
-void addHistogram(TString name, TString xLabel, int firstValue, int lastValue);
-
 void addHistogram(TString name, TString xLabel, int firstValue, int lastValue)
 {
   Histograms.push_back(name);
@@ -39,8 +37,6 @@ void addHistogram(TString name, TString xLabel, int firstValue, int lastValue)
 }
 
 // add selection step
-void addSelectionStep(TString step, TString selectionLabel);
-
 void addSelectionStep(TString step, TString selectionLabel)
 {
   Selections.push_back(step);
@@ -48,8 +44,6 @@ void addSelectionStep(TString step, TString selectionLabel)
 }
 
 // add bin
-void addBin(int firstBin, int lastBin, TString binLabel, int binColor, int marker);
-
 void addBin(int firstBin, int lastBin, TString binLabel, int binColor, int marker)
 {
   FirstBins.push_back(firstBin);
@@ -67,23 +61,13 @@ int TtGenEventAnalyzer_nJetsProjectionX()
   // Sample
   //--------------------------------------------------------------
 
-  TFile* TTJets = new TFile("SemiLepElMu.root", "READ");
-  //TFile* TTJets = new TFile("SingleTop.root", "READ");
+  //TFile* TTJets = new TFile("SemiLepElMu.root", "READ");
+  TFile* TTJets = new TFile("SingleTop.root", "READ");
   // TFile* TTJets = new TFile("DiLep.root", "READ");
 
   //--------------------------------------------------------------
   // addHistogram
   //--------------------------------------------------------------
-
-  //addHistogram("HT_MET",      "HT [GeV]",  "<MET> [GeV]");
-  //addHistogram("HT_LepPt",    "HT [GeV]",  "<Lepton p_{T}> [GeV]");
-
-  //addHistogram("HT_YMET",     "HT [GeV]",         "<YMET> [GeV^{1/2}]");
-  //addHistogram("HT_LepPtSig", "HT [GeV]",         "<Lepton p_{T} sig> [GeV^{1/2}]");
-  //addHistogram("HT_mT",       "HT [GeV]",         "<m_{T}> [GeV]");
-  //addHistogram("HT_mlb",        "HT [GeV]",       "mlb [GeV]");
-  //addHistogram("HT_mLepTop",    "HT [GeV]",       "mLepTop [GeV]");
-  //addHistogram("HT_minj3",        "HT [GeV]",  "minj3 [GeV]");
 
   //addHistogram("YMET_nJets",    "YMET [GeV^{1/2}]", 0, 25);
   addHistogram("mT_nJets",      "m_{T} [GeV]",      0, 300);
@@ -91,101 +75,18 @@ int TtGenEventAnalyzer_nJetsProjectionX()
   //addHistogram("mlb_nJets",     "mlb [GeV]",        0, 300);
   //addHistogram("mLepTop_nJets", "mLepTop [GeV]",    0, 400);
 
-  //addHistogram("pv_nJets",        "p_{#nu}  [GeV]",      0, 600);
-  //addHistogram("mlv_nJets_gen",   "m_{l#nu} [GeV]",      0, 200);
-  //addHistogram("mlv_nJets_reco",  "p_{l#nu} [GeV]",      0, 200);
-
-  //addHistogram("minj3_nJets",   "minj3 [GeV]");
-
-  //addHistogram("HT_MET_acceptance",      "HT [GeV]",  "<MET> [GeV]");
-  //addHistogram("HT_LepPt_acceptance",    "HT [GeV]",  "<Lepton p_{T}> [GeV]");
-  //addHistogram("HT_YMET_acceptance",     "HT [GeV]",  "<YMET> [GeV^{1/2}]");
-  //addHistogram("HT_LepPtSig_acceptance", "HT [GeV]",  "<Lepton p_{T} sig> [GeV^{1/2}]");
-  //addHistogram("HT_mT_acceptance",       "HT [GeV]",  "<m_{T}> [GeV]");
-
-//    addHistogram("qScale_MET", "genEvtInfoHandle->qScale()",  "<MET> [GeV]");
-//    addHistogram("qScale_HT", "genEvtInfoHandle->qScale()",  "<HT> [GeV]");
-
-//    addHistogram("PDFScale_MET", "genEvtInfoHandle->pdf()->scalePDF",  "<MET> [GeV]");
-//    addHistogram("PDFScale_HT", "genEvtInfoHandle->pdf()->scalePDF",  "<HT> [GeV]");
-
-//    addHistogram("ttbarMass_MET", "m_{t#bar{t}} [GeV]",  "<MET> [GeV]");
-//    addHistogram("ttbarMass_HT",  "m_{t#bar{t}} [GeV]",  "<HT> [GeV]");
-
   //--------------------------------------------------------------
   // addSelectionStep
   //--------------------------------------------------------------
-
-//   addSelectionStep("analyzeSUSY1l_leptonSelection_JER00", "lepton selection");
-//   addSelectionStep("analyzeSUSY1l_HTSelection_JER00",     "HT selection");
-//   addSelectionStep("analyzeSUSY1l_METSelection_JER00",    "MET selection");
-
-//   addSelectionStep("analyzeSUSY1l_leptonSelection_JER10", "lepton selection");
-//   addSelectionStep("analyzeSUSY1l_HTSelection_JER10",     "HT selection");
-//   addSelectionStep("analyzeSUSY1l_METSelection_JER10",    "MET selection");
-
-//   addSelectionStep("analyzeSUSY1l_leptonSelection_JER20", "lepton selection");
-//   addSelectionStep("analyzeSUSY1l_HTSelection_JER20",     "HT selection");
-//   addSelectionStep("analyzeSUSY1l_METSelection_JER20",    "MET selection");
-
-//   addSelectionStep("analyzeSUSY1l_leptonSelection_JER30", "lepton selection");
-//   addSelectionStep("analyzeSUSY1l_HTSelection_JER30",     "HT selection");
-//   addSelectionStep("analyzeSUSY1l_METSelection_JER30",    "MET selection");
-
-//   addSelectionStep("analyzeSUSY1l_leptonSelection_JER40", "lepton selection");
-//   addSelectionStep("analyzeSUSY1l_HTSelection_JER40",     "HT selection");
-//   addSelectionStep("analyzeSUSY1l_METSelection_JER40",    "MET selection");
-
-//   addSelectionStep("analyzeSUSY1l_leptonSelection_JER50", "lepton selection");
-//   addSelectionStep("analyzeSUSY1l_HTSelection_JER50",     "HT selection");
-//   addSelectionStep("analyzeSUSY1l_METSelection_JER50",    "MET selection");
-
   
-  addSelectionStep("analyzeCorrelation1l_YMET0To1", "lepton selection");
-  addSelectionStep("analyzeCorrelation1l_YMET1To2", "lepton selection");
-  addSelectionStep("analyzeCorrelation1l_YMET2To3", "lepton selection");
-  addSelectionStep("analyzeCorrelation1l_YMET3To4", "lepton selection");
-  addSelectionStep("analyzeCorrelation1l_YMET4To5", "lepton selection");
-  addSelectionStep("analyzeCorrelation1l_YMET5To6", "lepton selection");
+  addSelectionStep("analyzeCorrelation1l", "lepton selection");
 
-  //addSelectionStep("analyzeCorrelation1l_HT300ToInf", "lepton selection");
-  //addSelectionStep("analyzeCorrelation1l_HT400ToInf", "lepton selection");
-  //addSelectionStep("analyzeCorrelation1l_HT500ToInf", "lepton selection");
-
-//   addSelectionStep("analyzeCorrelation1l_HT200To300", "lepton selection");
-//   addSelectionStep("analyzeCorrelation1l_HT300To400", "lepton selection");
-//   addSelectionStep("analyzeCorrelation1l_HT400To500", "lepton selection");
-  //addSelectionStep("analyzeCorrelation1l_HT500To600", "lepton selection");
-//   addSelectionStep("analyzeCorrelation1l_HT600ToInf", "lepton selection");
-
-//   addSelectionStep("analyzeCorrelation1l_MET0To50",   "lepton selection");
-//   addSelectionStep("analyzeCorrelation1l_MET50To100", "lepton selection");
-  //addSelectionStep("analyzeCorrelation1l_MET100To150", "lepton selection");
-//   addSelectionStep("analyzeCorrelation1l_MET150To200", "lepton selection");
-//   addSelectionStep("analyzeCorrelation1l_MET200To250", "lepton selection");
-//   addSelectionStep("analyzeCorrelation1l_MET250ToInf", "lepton selection");
-
-  //addSelectionStep("analyzeCorrelation1l", "lepton selection");
-  //addSelectionStep("analyzeCorrelation1l_MET100ToInf", "lepton selection");
-
- //  addSelectionStep("analyzeCorrelation1l_YMET0To3", "lepton selection");
-//   addSelectionStep("analyzeCorrelation1l_YMET3To5", "lepton selection");
-//   addSelectionStep("analyzeCorrelation1l_YMET5To8", "lepton selection");
-
-  //addSelectionStep("analyzeCorrelation1l_HT400ToInf", "lepton selection");
-  //addSelectionStep("analyzeCorrelation1l_HT500ToInf", "lepton selection");
-  //addSelectionStep("analyzeCorrelation1l_MET100ToInf", "lepton selection");
-  //addSelectionStep("analyzeCorrelation1l_HT300ToInf", "lepton selection");
-
-  //addSelectionStep("analyzeCorrelation1l_HT300ToInf_MET100ToInf", "lepton selection");
-//   addSelectionStep("analyzeCorrelation1l_HT300ToInf_MET150ToInf", "lepton selection");
-//   addSelectionStep("analyzeCorrelation1l_HT300ToInf_MET200ToInf", "lepton selection");
-
-//   addSelectionStep("analyzeCorrelation1l_HT300ToInf", "lepton selection");
-//   addSelectionStep("analyzeCorrelation1l_HT300ToInf_MET0To100",   "lepton selection");
-//   addSelectionStep("analyzeCorrelation1l_HT300ToInf_MET100To200", "lepton selection");
-//   addSelectionStep("analyzeCorrelation1l_HT300ToInf_MET200To300", "lepton selection");
-//   addSelectionStep("analyzeCorrelation1l_HT300ToInf_MET300ToInf", "lepton selection");
+//   addSelectionStep("analyzeCorrelation1l_YMET0To1", "lepton selection");
+//   addSelectionStep("analyzeCorrelation1l_YMET1To2", "lepton selection");
+//   addSelectionStep("analyzeCorrelation1l_YMET2To3", "lepton selection");
+//   addSelectionStep("analyzeCorrelation1l_YMET3To4", "lepton selection");
+//   addSelectionStep("analyzeCorrelation1l_YMET4To5", "lepton selection");
+//   addSelectionStep("analyzeCorrelation1l_YMET5To6", "lepton selection");
 
   //--------------------------------------------------------------
   // addBin
@@ -216,12 +117,24 @@ int TtGenEventAnalyzer_nJetsProjectionX()
 	  
 	  TCanvas *canvas = new TCanvas(Selections[sdx]+"_"+Histograms[hdx],Selections[sdx]+"_"+Histograms[hdx], 1);
 
-	  TLegend *leg = new TLegend(.6,.56,.91,.89);
+	  // legend
+	  TLegend *leg = new TLegend(.56,.67,.90,.91);
 	  leg->SetTextFont(42);
+	  leg->SetTextSize(0.055);
 	  leg->SetFillColor(0);
 	  leg->SetLineColor(1);
 	  leg->SetShadowColor(0);
+	  leg->SetLineColor(0);
 	  leg->AddEntry((TObject*)0, "Semilep. t#bar{t}", "");
+
+	  // label
+	  TPaveText *label = new TPaveText(0.14,0.94,0.99,1.,"NDC");
+	  label->SetFillColor(0);
+	  label->SetTextFont(42);
+	  label->SetTextSize(0.043);
+	  label->SetBorderSize(0);
+	  label->SetTextAlign(12);
+	  TText *text=label->AddText("Simulation, #sqrt{s} = 7 TeV");
       
 	  TH2F* Hist = (TH2F*)TTJets->Get(Selections[sdx]+"/"+Histograms[hdx]);
 
@@ -232,31 +145,35 @@ int TtGenEventAnalyzer_nJetsProjectionX()
 	      // create projection
 	      TH1F* Projection = (TH1F*)Hist->ProjectionX(Histograms[hdx], FirstBins[bin], LastBins[bin],"");
 	      
-	      // edit projection
+	      // titles, scales and ranges
 	      Projection->SetTitle("");
+
 	      Projection->GetXaxis()->SetTitle(XLabels[hdx]);
-	      Projection->GetXaxis()->SetTitleSize(0.07);
+	      Projection->GetXaxis()->SetTitleSize(0.05);
+	      Projection->GetYaxis()->SetTitleOffset(1.4);
 	      Projection->GetXaxis()->SetRangeUser(FirstValues[hdx],LastValues[hdx]);
-	      Projection->GetYaxis()->SetTitle("events");
+	      
+	      Projection->GetYaxis()->SetTitle("a.u.");
+	      Projection->GetYaxis()->SetTitleSize(0.05);
+	      Projection->GetYaxis()->SetTitleSize(0.05);
+
+	      Projection->Scale(1/Projection->Integral(1,-1));
+
+	      // lines
 	      Projection->SetLineColor(BinColors[bin]);
 	      Projection->SetLineWidth(2);
-	      Projection->Scale(1/Projection->Integral(1,-1));
+
+	      // markers
 	      Projection->SetMarkerStyle(MarkerStyles[bin]);
 	      Projection->SetMarkerColor(BinColors[bin]);
+
+	      // add entry to legend
 	      leg->AddEntry(Projection->Clone(),BinLabels[bin],"l P");
 
 	      if(bin == 0) Projection->DrawCopy();
 	      else Projection->DrawCopy("same");
 	    }
-	  TPaveText *label = new TPaveText(0.11,0.94,0.99,1.,"NDC");
-	  label->SetFillColor(0);
-	  label->SetTextFont(42);
-	  label->SetTextSize(0.043);
-	  label->SetBorderSize(0);
-	  label->SetTextAlign(12);
-	  TText *text=label->AddText("Simulation, #sqrt{s} = 7 TeV");
-	  label->Draw("same");
-	  
+
 	  TPaveText *label2 = new TPaveText(0.45,0.38,0.75,0.54,"NDC");
 	  label2->SetFillColor(0);
 	  label2->SetTextFont(62);
@@ -265,47 +182,13 @@ int TtGenEventAnalyzer_nJetsProjectionX()
 	  label2->SetTextAlign(12);
 	  TText *text2=label2->AddText("Own work");
 	  TText *text3=label2->AddText("in progress");
-	  //label2->Draw("same");
 	  
+	  label->Draw("same");
+	  //label2->Draw("same");
 	  leg->Draw();
 
 	  canvas->SetLogy();
 	  canvas->SaveAs(Selections[sdx]+"_"+Histograms[hdx]+"_ProjectionX_log.pdf");
 	}
-    }
-
-// 	  TCanvas *canvas = new TCanvas(Histograms[hdx],Histograms[hdx],1);
-	  
-// 	  TLegend *leg = new TLegend(.64,.18,.91,.45);
-// 	  leg->SetTextFont(42);
-// 	  leg->SetFillColor(0);
-// 	  leg->SetLineColor(1);
-// 	  leg->SetShadowColor(0);
-	  
-// 	  for(int bin=0; bin<(int)bins.size(); ++bin)
-// 	    {
-// 	      std::cout << "bin " << bin << std::cout;
-	      
-// 	      // create projection
-// 	      TH2F* Hist = (TH2F*)TTJets->Get(Selections[sdx]+"/"+Histograms[hdx]);
-// 	      TH1F* Projection = (TH1F*)Hist->ProjectionX("", bins[bin], bins[bin],"");
-	      
-// 	      // edit projection
-// 	      Projection->SetTitle("");
-// 	      Projection->GetXaxis()->SetTitle(XLabels[hdx]);
-// 	      Projection->GetXaxis()->SetTitleOffset(1.4);
-// 	      Projection->GetXaxis()->SetRangeUser(0,400);
-// 	      Projection->GetYaxis()->SetTitle("# events");
-// 	      Projection->SetLineColor(LineColors[sdx]);
-// 	      Projection->SetLineWidth(2);
-// 	      leg->AddEntry(Projection->Clone(),Labels[sdx],"l P");
-	      
-// 	      if(bin == 0) Projection->DrawCopy();
-// 	      else Projection->DrawCopy("same");
-	      // }
-	  //leg->Draw();
-	  // 	  canvas->SetLogy();
-	  
-	  // 	  canvas->SaveAs(Histograms[hdx]+"_reco.pdf");
-	  
+    }	  
 }
