8c169f430d8a555790087fde35164b4b06a48c05
npietsch <>
2013-06-13 14:24:29 +0000

    update

---
+++ SUSYAnalysis/SUSYObjects/src/HerwigGenEvent.cc
@@ -0,0 +1,13 @@
+#include "FWCore/Utilities/interface/EDMException.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "CommonTools/CandUtils/interface/pdgIdUtils.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+#include "SUSYAnalysis/SUSYObjects/interface/HerwigGenEvent.h"
+
+
+/// default contructor
+HerwigGenEvent::HerwigGenEvent(reco::GenParticleRefProd& decaySubset)
+{
+  parts_ = decaySubset;
+}
