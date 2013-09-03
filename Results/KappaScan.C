5bf16076a9c3fbcfbfcc662f515b7bdb5fa45077
npietsch <>
2013-09-03 08:43:33 +0000

    update

--- Results/KappaScan.C
+++ Results/KappaScan.C
@@ -20,9 +20,11 @@ vector<TString> Names;
 
 vector<TString> Histograms;
 vector<TString> XLabels;
-vector<unsigned int> FirstValues;
-vector<unsigned int> LastValues;
-vector<unsigned int> DrawLegend;
+vector<TString> YLabels;
+vector<unsigned int> LowerBinsX;
+vector<unsigned int> UpperBinsX;
+vector<unsigned int> LowerBinsY;
+vector<unsigned int> UpperBinsY;
 
 vector<TString> Modules;
 vector<TString> Selections;
@@ -30,11 +32,6 @@ vector<TString> Channels;
 vector<TString> SelectionLabels;
 vector<TString> ChannelLabels;
 
-vector<unsigned int> FirstBins;
-vector<unsigned int> LastBins;
-vector<TString> BinLabels;
-vector<unsigned int> BinColors;
-vector<unsigned int> MarkerStyles;
 
 // addSample
 void addSample(TFile* file, TString sample, TString name)
@@ -45,13 +42,15 @@ void addSample(TFile* file, TString sample, TString name)
 }
 
 // add histogram
-void addHistogram(TString name, TString xLabel, int firstValue, int lastValue, int drawLegend)
+void addHistogram(TString name, TString xLabel, TString yLabel, int lowerBinX, int upperBinX, int lowerBinY, int upperBinY )
 {
   Histograms.push_back(name);
   XLabels.push_back(xLabel);
-  FirstValues.push_back(firstValue);
-  LastValues.push_back(lastValue);
-  DrawLegend.push_back(drawLegend);
+  YLabels.push_back(yLabel);
+  LowerBinsX.push_back(lowerBinX);
+  UpperBinsX.push_back(upperBinX);
+  LowerBinsY.push_back(lowerBinY);
+  UpperBinsY.push_back(upperBinY);
 }
 
 // add selection step
@@ -69,27 +68,15 @@ void addChannel(TString channel, TString channelLabel)
   ChannelLabels.push_back(channelLabel);
 }
 
-// add bin
-void addBin(int firstBin, int lastBin, TString binLabel, int binColor, int marker)
-{
-  FirstBins.push_back(firstBin);
-  LastBins.push_back(lastBin);
-  BinLabels.push_back(binLabel);
-  BinColors.push_back(binColor);
-  MarkerStyles.push_back(marker);
-}
-
 // main function
 int KappaScan()
 {
-  TFile* WJetsHT = new TFile("WJetsHT.root",      "READ");
-  //TFile* TTJets  = new TFile("TTJetsFall11.root", "READ");
-  TFile* SemiLepElMu = new TFile("SemiLepElMuTTJets.root", "READ");
-  TFile* SemiLepTau  = new TFile("SemiLepTauTTJets.root",  "READ");
-  TFile* DiLep       = new TFile("DiLepTTJets.root", "READ");
-  TFile* FullHad     = new TFile("FullHadTTJets.root", "READ");
-  TFile* SingleTop   = new TFile("SingleTop.root", "READ");
+  bool SeparateChannels = true;
 
+
+  // files
+   TFile* SemiLepElMu = new TFile("SemiLepElMuTTJets.root", "READ");
+   
   //--------------------------------------------------------------------------------------------------
   // add addSample(TFile* file, TString sample, TString name)
   //--------------------------------------------------------------------------------------------------
@@ -100,14 +87,13 @@ int KappaScan()
   // addHistogram(TString name, TString xLabel, int firstValue, int lastValue, int drawLegend) 
   //--------------------------------------------------------------------------------------------------
   
-  addHistogram("HT_YMET",     "H_{T} [GeV]", 360, 1000, 1);
-  //addHistogram("HT_LepPtSig", "H_{T} [GeV]", 360, 1000, 0);
+  addHistogram("HT_YMET", "H_{T} [GeV]", "Y_{MET} [GeV^{#frac{1}{2}}]", 10, 41, 7, 51);
   
   //--------------------------------------------------------------------------------------------------
   // addSelectionStep(TString module, TString step, TString selectionLabel)
   //--------------------------------------------------------------------------------------------------
   
-  addSelectionStep("analyzeSUSY1", "_leptonSelection", "lepton selection");
+  //addSelectionStep("analyzeSUSY1", "_leptonSelection", "lepton selection");
   addSelectionStep("analyzeSUSY1", "_jetSelection",    "jet selection");
   
   //--------------------------------------------------------------------------------------------------
@@ -115,7 +101,7 @@ int KappaScan()
   //--------------------------------------------------------------------------------------------------
   
   addChannel("m", "muon channel");
-  addChannel("e", "electron channel");
+  //addChannel("e", "electron channel");
   
   //--------------------------------------------------------------------------------------------------
   // Set style 
@@ -144,14 +130,6 @@ int KappaScan()
 		      
 		      TCanvas *canvas = new TCanvas(Modules[sdx]+Channels[cdx]+Selections[sdx]+"_"+Histograms[hdx]+"_"+Samples[fdx],Modules[sdx]+Channels[cdx]+Selections[sdx]+"_"+Histograms[hdx]+"_"+Samples[fdx], 1);
 		      
-		      TLegend *leg = new TLegend(.55,.62,.95,.93);
-		      leg->SetTextFont(42);
-		      leg->SetTextSize(0.05);
-		      //leg->SetTextSize(0.04);
-		      leg->SetFillColor(0);
-		      leg->SetLineColor(1);
-		      leg->SetShadowColor(0);
-		      
 		      TPaveText *label = new TPaveText(0.14,0.94,0.99,1.,"NDC");
 		      label->SetFillColor(0);
 		      label->SetTextFont(42);
@@ -162,89 +140,33 @@ int KappaScan()
 		      
 		      TH2F* Hist = (TH2F*)Files[fdx]->Get(Modules[sdx]+Channels[cdx]+Selections[sdx]+"/"+Histograms[hdx]);
 		      
-		      for(int bin=0; bin<(int)FirstBins.size(); ++bin)
-			{
-			  std::cout << "bins " << FirstBins[bin] << " - " << LastBins[bin] << std::endl;
-			  
-			  // create projection
-			  TH1F* Projection = (TH1F*)Hist->ProjectionX(Histograms[hdx], FirstBins[bin], LastBins[bin],"");
-			  
-			  // edit ranges and scale
-			  Projection->GetXaxis()->SetRangeUser(FirstValues[hdx],LastValues[hdx]);
-			  Projection->Scale(1/Projection->Integral(NormBin,-1));
-			  			  if(Log == true)
-			    {
-			      Projection->SetMinimum(LogMin);
-			      Projection->SetMaximum(LogMax);
-			    }
-			  else
-			    {
-			      Projection->SetMinimum(Min);
-			      Projection->SetMaximum(Max);
-			    } 
+		      std::cout << LowerBinsX[hdx] << std::endl;
+		      std::cout << UpperBinsX[hdx] << std::endl;
 
-			  // edit titles
-			  Projection->SetTitle("");
-			  
-			  Projection->GetXaxis()->SetTitle(XLabels[hdx]);
-			  Projection->GetXaxis()->SetTitleOffset(1.2);
-			  Projection->GetXaxis()->SetTitleSize(0.05);
-			  Projection->GetXaxis()->SetTitleFont(42);
-			  Projection->GetXaxis()->SetLabelFont(42);
-			  
-			  Projection->GetYaxis()->SetTitle("a.u.");
-			  Projection->GetYaxis()->SetTitleOffset(1.4);
-			  Projection->GetYaxis()->SetTitleSize(0.05);
-			  Projection->GetYaxis()->SetTitleFont(42);
-			  Projection->GetYaxis()->SetLabelFont(42);
-			  
-			  // edit lines and marker
-			  Projection->SetLineColor(BinColors[bin]);
-			  Projection->SetLineWidth(2);
-			  Projection->SetMarkerStyle(MarkerStyles[bin]);
-			  Projection->SetMarkerColor(BinColors[bin]);
-			  
-			  // add entry to leg
-			  leg->AddEntry(Projection->Clone(),BinLabels[bin],"l P");
-			  
-			  if(bin == 0) Projection->DrawCopy();
-			  else Projection->DrawCopy("same");
+		      TH2F* Scan=new TH2F("Scan", "Scan", 40, 0, 2000, 50, 0, 25);
+
+		      for(int binX=13; binX<=25; ++binX)
+			{
+
+			  for(int binY=10; binY<=25; ++binY)
+			    {
+			      //std::cout << "x bin: " << binX << ", y bin: " << binY << std::endl;
+
+			      double nA = Hist->Integral(LowerBinsX[hdx], binX,            LowerBinsY[hdx], binY);
+			      double nB = Hist->Integral(binX,            UpperBinsX[hdx], LowerBinsY[hdx], binY);
+			      double nC = Hist->Integral(LowerBinsX[hdx], binX,            binY,       UpperBinsY[hdx]);
+			      double nD = Hist->Integral(binX,            UpperBinsX[hdx], binY,       UpperBinsY[hdx]);
+
+			      double Kappa=(nD*nA)/(nB*nC);
+			      
+			      //std::cout << Kappa << std::endl;
+
+			      Scan->SetBinContent(binX, binY, Kappa);
+			    }
 			}
 		      
-		      // draw legend
-		      if(DrawLegend[hdx] == 1) leg->Draw();
-		  
-		      // draw labels
-		      label->Draw();
-		      	
-		      // draw TLine
-		      if(Log == true)
-			{
-			  TLine * line = new TLine(3, LogMin, 3, LogMax);
-			  line->SetLineWidth(2);
-			  line->SetLineStyle(2);
-			  line->SetLineColor(1);
-			  line->Draw();
-			}
-		      else
-			{
-			  TLine * line = new TLine(3, Min, 3, Max);
-			  line->SetLineWidth(2);
-			  line->SetLineStyle(2);
-			  line->SetLineColor(1);
-			  line->Draw();
-			}
-		      
-		      // save canvas
-		      if(Log == true)
-			{
-			  canvas->SetLogy();
-			  canvas->SaveAs(Modules[sdx]+Channels[cdx]+Selections[sdx]+"_"+Histograms[hdx]+"_ProjectionX_"+Samples[fdx]+"_log.pdf");
-			}
-		      else
-			{
-			  canvas->SaveAs(Modules[sdx]+Channels[cdx]+Selections[sdx]+"_"+Histograms[hdx]+"_ProjectionX_"+Samples[fdx]+".pdf");
-			}
+		      Scan->Draw();
+		      canvas->SaveAs(Modules[sdx]+Channels[cdx]+Selections[sdx]+"_"+Histograms[hdx]+"_ProjectionX_"+Samples[fdx]+".pdf");                      
 		    }
 		}
 	    }      
