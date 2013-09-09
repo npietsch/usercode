5dd76195e51d1b5eb5dc915463dffd55530ab881
npietsch <>
2013-09-09 18:42:55 +0000

    update

--- Results/Colz.C
+++ Results/Colz.C
@@ -25,7 +25,7 @@ vector<double> LowerValuesX;
 vector<double> HigherValuesX;
 vector<double> LowerValuesY;
 vector<double> HigherValuesY;
-vector<unsigned int> DrawLegend;
+vector<TString> HistogramLabels;
 
 vector<TString> Modules;
 vector<TString> Selections;
@@ -48,7 +48,7 @@ void addSample(TFile* file, TString sample, TString name)
 }
 
 // add histogram
-void addHistogram(TString name, TString xLabel, TString yLabel, double lowerValueX, double higherValueX, double lowerValueY, double higherValueY, int drawLegend)
+void addHistogram(TString name, TString xLabel, TString yLabel, double lowerValueX, double higherValueX, double lowerValueY, double higherValueY, TString histogramLabel)
 {
   Histograms.push_back(name);
   XLabels.push_back(xLabel);
@@ -57,7 +57,7 @@ void addHistogram(TString name, TString xLabel, TString yLabel, double lowerValu
   HigherValuesX.push_back(higherValueX);
   LowerValuesY.push_back(lowerValueY);
   HigherValuesY.push_back(higherValueY);
-  DrawLegend.push_back(drawLegend);
+  HistogramLabels.push_back(histogramLabel);
 }
 
 // add selection step
@@ -78,12 +78,12 @@ void addChannel(TString channel, TString channelLabel)
 // main function
 int Colz()
 {
-  bool Log = false;
+  bool Log = true;
   bool SeparateChannels = false;
   bool CombineChannels = true;
 
   double LogMin=0.001;
-  double LogMax=0.8;
+  double LogMax=0.6;
 
   double Min=0;
   double Max=0.5;
@@ -113,14 +113,17 @@ int Colz()
   // addHistogram(TString name, TString xLabel, int firstValue, int lastValue, int drawLegend) 
   //--------------------------------------------------------------------------------------------------
   
-  addHistogram("NuPt_fakeMET",  "P_{T}^{#nu}", "#slash{E}_{T}^{fake} [GeV]", 0, 500, 0, 500, 1);
+  addHistogram("NuPt_fakeMET_375HT500",  "P_{T}^{#nu} [GeV]", "#slash{E}_{T}^{fake} [GeV]", 0, 400, 0, 200, "375 < H_{T} < 500");
+  addHistogram("NuPt_fakeMET_500HT650",  "P_{T}^{#nu} [GeV]", "#slash{E}_{T}^{fake} [GeV]", 0, 400, 0, 200, "500 < H_{T} < 650");
+  addHistogram("NuPt_fakeMET_650HT800",  "P_{T}^{#nu} [GeV]", "#slash{E}_{T}^{fake} [GeV]", 0, 400, 0, 200, "650 < H_{T} < 800");
+  addHistogram("NuPt_fakeMET_800HT950",  "P_{T}^{#nu} [GeV]", "#slash{E}_{T}^{fake} [GeV]", 0, 400, 0, 200, "800 < H_{T} < 950");
   
   //--------------------------------------------------------------------------------------------------
   // addSelectionStep(TString module, TString step, TString selectionLabel)
   //--------------------------------------------------------------------------------------------------
   
-  addSelectionStep("analyzeSUSY1", "_leptonSelection", "lepton selection");
-  addSelectionStep("analyzeSUSY1", "_jetSelection",    "jet selection");
+  //addSelectionStep("analyzeCorrelation1", "_leptonSelection", "lepton selection");
+  addSelectionStep("analyzeCorrelation1", "_jetSelection",    "jet selection");
 
   //--------------------------------------------------------------------------------------------------
   // addChannel(TString channel, TString channelLabel)
@@ -134,7 +137,15 @@ int Colz()
   //--------------------------------------------------------------------------------------------------
 
   setTDRStyle();
-  
+  tdrStyle->SetPadLeftMargin(0.1);
+  tdrStyle->SetPadRightMargin(0.14);
+  tdrStyle->SetPadTopMargin(0.1);
+  tdrStyle->SetPadBottomMargin(0.14);
+  tdrStyle->SetPalette(1);
+  tdrStyle->SetCanvasDefH(400);
+  tdrStyle->SetCanvasDefW(800);
+
+
   //--------------------------------------------------------------------------------------------------
   // Plot
   //--------------------------------------------------------------------------------------------------
@@ -146,119 +157,170 @@ int Colz()
 	  std::cout << "\n" << Histograms[hdx] << std::endl;
 	  std::cout << "-----------------------" << std::endl;
 	  
-// 	  for(int sdx=0; sdx<(int)Selections.size(); ++sdx)
-// 	    {
-// 	      if(SeparateChannels == true)
-// 		{
-// 		  for(int cdx=0; cdx<(int)Channels.size(); ++cdx)
-// 		    {
-// 		      std::cout << Modules[sdx]+Channels[cdx]+Selections[sdx] << "_" << Histograms[hdx] << std::endl;
+	  for(int sdx=0; sdx<(int)Selections.size(); ++sdx)
+	    {
+	      if(SeparateChannels == true)
+		{
+		  for(int cdx=0; cdx<(int)Channels.size(); ++cdx)
+		    {
+ 		      std::cout << Modules[sdx]+Channels[cdx]+Selections[sdx] << "_" << Histograms[hdx] << std::endl;
 		      
-// 		      TCanvas *canvas = new TCanvas(Modules[sdx]+Channels[cdx]+Selections[sdx]+"_"+Histograms[hdx]+"_"+Samples[fdx],Modules[sdx]+Channels[cdx]+Selections[sdx]+"_"+Histograms[hdx]+"_"+Samples[fdx], 1);
-		      
-// 		      TLegend *leg = new TLegend(.55,.62,.95,.93);
-// 		      leg->SetTextFont(42);
-// 		      leg->SetTextSize(0.05);
-// 		      //leg->SetTextSize(0.04);
-// 		      leg->SetFillColor(0);
-// 		      leg->SetLineColor(1);
-// 		      leg->SetShadowColor(0);
-		      
-// 		      TPaveText *label = new TPaveText(0.14,0.94,0.99,1.,"NDC");
-// 		      label->SetFillColor(0);
-// 		      label->SetTextFont(42);
-// 		      label->SetTextSize(0.043);
-// 		      label->SetBorderSize(0);
-// 		      label->SetTextAlign(12);
-// 		      TText *text=label->AddText("Simulation, #sqrt{s} = 7 TeV, "+ChannelLabels[cdx]);
-		      
-// 		      TH2F* Hist = (TH2F*)Files[fdx]->Get(Modules[sdx]+Channels[cdx]+Selections[sdx]+"/"+Histograms[hdx]);
-		      
-// 		      for(int bin=0; bin<(int)FirstBins.size(); ++bin)
-// 			{
-// 			  std::cout << "bins " << FirstBins[bin] << " - " << LastBins[bin] << std::endl;
-			  
-// 			  // create projection
-// 			  TH1F* Projection = (TH1F*)Hist->ProjectionX(Histograms[hdx], FirstBins[bin], LastBins[bin],"");
-			  
-// 			  // edit ranges and scale
-// 			  Projection->GetXaxis()->SetRangeUser(FirstValues[hdx],LastValues[hdx]);
-// 			  Projection->Scale(1/Projection->Integral(NormBin,-1));
-// 			  			  if(Log == true)
-// 			    {
-// 			      Projection->SetMinimum(LogMin);
-// 			      Projection->SetMaximum(LogMax);
-// 			    }
-// 			  else
-// 			    {
-// 			      Projection->SetMinimum(Min);
-// 			      Projection->SetMaximum(Max);
-// 			    } 
+		      TCanvas *canvas = new TCanvas(Modules[sdx]+Channels[cdx]+Selections[sdx]+"_"+Histograms[hdx]+"_"+Samples[fdx],Modules[sdx]+Channels[cdx]+Selections[sdx]+"_"+Histograms[hdx]+"_"+Samples[fdx], 1);
 
-// 			  // edit titles
-// 			  Projection->SetTitle("");
-			  
-// 			  Projection->GetXaxis()->SetTitle(XLabels[hdx]);
-// 			  Projection->GetXaxis()->SetTitleOffset(1.2);
-// 			  Projection->GetXaxis()->SetTitleSize(0.05);
-// 			  Projection->GetXaxis()->SetTitleFont(42);
-// 			  Projection->GetXaxis()->SetLabelFont(42);
-			  
-// 			  Projection->GetYaxis()->SetTitle("a.u.");
-// 			  Projection->GetYaxis()->SetTitleOffset(1.4);
-// 			  Projection->GetYaxis()->SetTitleSize(0.05);
-// 			  Projection->GetYaxis()->SetTitleFont(42);
-// 			  Projection->GetYaxis()->SetLabelFont(42);
-			  
-// 			  // edit lines and marker
-// 			  Projection->SetLineColor(BinColors[bin]);
-// 			  Projection->SetLineWidth(2);
-// 			  Projection->SetMarkerStyle(MarkerStyles[bin]);
-// 			  Projection->SetMarkerColor(BinColors[bin]);
-			  
-// 			  // add entry to leg
-// 			  leg->AddEntry(Projection->Clone(),BinLabels[bin],"l P");
-			  
-// 			  if(bin == 0) Projection->DrawCopy();
-// 			  else Projection->DrawCopy("same");
-// 			}
+		      TPaveText *label = new TPaveText(0.14,0.94,0.99,1.,"NDC");
+		      label->SetFillColor(0);
+		      label->SetTextFont(42);
+		      label->SetTextSize(0.043);
+		      label->SetBorderSize(0);
+		      label->SetTextAlign(12);
+		      TText *text=label->AddText("Simulation, #sqrt{s} = 7 TeV, "+ChannelLabels[cdx]);
 		      
-// 		      // draw legend
-// 		      if(DrawLegend[hdx] == 1) leg->Draw();
-		  
-// 		      // draw labels
-// 		      label->Draw();
-		      	
-// 		      // draw TLine
+ 		      TH2F* Hist = (TH2F*)Files[fdx]->Get(Modules[sdx]+Channels[cdx]+Selections[sdx]+"/"+Histograms[hdx]);
+
+		      TProfile2D* Profile = (TProfile2D*)Hist->ProfileX();
+
+// 		      // Set Ranges
 // 		      if(Log == true)
 // 			{
-// 			  TLine * line = new TLine(3, LogMin, 3, LogMax);
-// 			  line->SetLineWidth(2);
-// 			  line->SetLineStyle(2);
-// 			  line->SetLineColor(1);
-// 			  line->Draw();
+// 			  Hist->SetMinimum(LogMin);
+// 			  Hist->SetMaximum(LogMax);
 // 			}
 // 		      else
 // 			{
-// 			  TLine * line = new TLine(3, Min, 3, Max);
-// 			  line->SetLineWidth(2);
-// 			  line->SetLineStyle(2);
-// 			  line->SetLineColor(1);
-// 			  line->Draw();
-// 			}
+// 			  Hist->SetMinimum(Min);
+// 			  Hist->SetMaximum(Max);
+// 			} 
+		      Hist->GetXaxis()->SetRangeUser(LowerValuesX[hdx],HigherValuesX[hdx]);
+		      Hist->GetYaxis()->SetRangeUser(LowerValuesY[hdx],HigherValuesY[hdx]);
 		      
-// 		      // save canvas
+		      // edit titles
+		      Hist->SetTitle("");
+		      
+		      Hist->GetXaxis()->SetTitle(XLabels[hdx]);
+		      Hist->GetXaxis()->SetTitleOffset(1.1);
+		      Hist->GetXaxis()->SetTitleSize(0.05);
+		      Hist->GetXaxis()->SetTitleFont(42);
+		      Hist->GetXaxis()->SetLabelFont(42);
+		      
+		      Hist->GetYaxis()->SetTitle(YLabels[hdx]);
+		      Hist->GetYaxis()->SetTitleOffset(0.8);
+		      Hist->GetYaxis()->SetTitleSize(0.05);
+		      Hist->GetYaxis()->SetTitleFont(42);
+		      Hist->GetYaxis()->SetLabelFont(42);
+		      
+		      Hist->GetZaxis()->SetTitle("Events");
+		      Hist->GetZaxis()->SetTitleOffset(0.6);
+		      Hist->GetZaxis()->SetTitleSize(0.05);
+		      Hist->GetZaxis()->SetTitleFont(42);
+		      Hist->GetZaxis()->SetLabelFont(42);
+
+		      Hist->Draw("colz");
+		      
+		      Profile->SetLineColor(1);
+		      Profile->SetLineWidth(2);
+		      Profile->Draw("same");
+		      
+ 		      // draw labels
+ 		      label->Draw();
+		  		      
+		      // save canvas
+		      if(Log == true)
+			{
+			  canvas->SetLogz();
+			  canvas->SaveAs(Modules[sdx]+Channels[cdx]+Selections[sdx]+"_"+Histograms[hdx]+"_"+Samples[fdx]+"_log.pdf");
+			}
+		      else
+			{
+			  canvas->SaveAs(Modules[sdx]+Channels[cdx]+Selections[sdx]+"_"+Histograms[hdx]+"_"+Samples[fdx]+".pdf");
+			}
+		    }	
+		}
+
+	      if(CombineChannels == true)
+		{
+		  std::cout << Modules[sdx]+"l"+Selections[sdx] << "_" << Histograms[hdx] << std::endl;
+		      
+		      TCanvas *canvas = new TCanvas(Modules[sdx]+"l"+Selections[sdx]+"_"+Histograms[hdx]+"_"+Samples[fdx],Modules[sdx]+"l"+Selections[sdx]+"_"+Histograms[hdx]+"_"+Samples[fdx], 1);
+		      
+		      TPaveText *label = new TPaveText(0.1,0.92,0.96,1.,"NDC");
+		      label->SetFillColor(0);
+		      label->SetTextFont(42);
+		      label->SetTextSize(0.06);
+		      label->SetBorderSize(0);
+		      label->SetTextAlign(12);
+		      TText *text=label->AddText("Simulation, #sqrt{s} = 7 TeV, "+HistogramLabels[hdx]);
+		      
+ 		      TH2F* Hist = (TH2F*)Files[fdx]->Get(Modules[sdx]+Channels[0]+Selections[sdx]+"/"+Histograms[hdx]);
+
+		      for(int cfx=0; cdx<(int)Channels.size(); ++cdx)
+			{
+			  
+			  TH2F* Hist2 = (TH2F*)Files[fdx]->Get(Modules[sdx]+Channels[cfx]+Selections[sdx]+"/"+Histograms[hdx]);
+			  Hist->Add(Hist2);
+			}
+		      TProfile2D* Profile = (TProfile2D*)Hist->ProfileX();
+			  
+			  
+// 		      // Set Ranges
 // 		      if(Log == true)
 // 			{
-// 			  canvas->SetLogy();
-// 			  canvas->SaveAs(Modules[sdx]+Channels[cdx]+Selections[sdx]+"_"+Histograms[hdx]+"_ProjectionX_"+Samples[fdx]+"_log.pdf");
+// 			  Hist->SetMinimum(LogMin);
+// 			  Hist->SetMaximum(LogMax);
 // 			}
 // 		      else
 // 			{
-// 			  canvas->SaveAs(Modules[sdx]+Channels[cdx]+Selections[sdx]+"_"+Histograms[hdx]+"_ProjectionX_"+Samples[fdx]+".pdf");
-// 			}
-// 		    }
-// 		}
+// 			  Hist->SetMinimum(Min);
+// 			  Hist->SetMaximum(Max);
+// 			} 
+		      Hist->GetXaxis()->SetRangeUser(LowerValuesX[hdx],HigherValuesX[hdx]);
+		      Hist->GetYaxis()->SetRangeUser(LowerValuesY[hdx],HigherValuesY[hdx]);
+		      
+		      // edit titles
+		      Hist->SetTitle("");
+		      
+		      Hist->GetXaxis()->SetTitle(XLabels[hdx]);
+		      Hist->GetXaxis()->SetTitleOffset(0.9);
+		      Hist->GetXaxis()->SetTitleSize(0.07);
+		      Hist->GetXaxis()->SetTitleFont(42);
+		      Hist->GetXaxis()->SetLabelFont(42);
+		      Hist->GetXaxis()->SetLabelSize(0.05);
+		      
+		      Hist->GetYaxis()->SetTitle(YLabels[hdx]);
+		      Hist->GetYaxis()->SetTitleOffset(0.7);
+		      Hist->GetYaxis()->SetTitleSize(0.07);
+		      Hist->GetYaxis()->SetTitleFont(42);
+		      Hist->GetYaxis()->SetLabelFont(42);
+		      Hist->GetYaxis()->SetLabelSize(0.05);
+
+		      Hist->GetZaxis()->SetTitle("Events");
+		      Hist->GetZaxis()->SetTitleOffset(0.6);
+		      Hist->GetZaxis()->SetTitleSize(0.07);
+		      Hist->GetZaxis()->SetTitleFont(42);
+		      Hist->GetZaxis()->SetLabelFont(42);
+		      Hist->GetZaxis()->SetLabelSize(0.05);
+
+		      Hist->Draw("colz");
+		      
+		      Profile->SetLineColor(1);
+		      Profile->SetLineWidth(3);
+		      Profile->Draw("same");
+		      
+ 		      // draw labels
+ 		      label->Draw();
+		  		      
+		      // save canvas
+		      if(Log == true)
+			{
+			  canvas->SetLogz();
+			  canvas->SaveAs(Modules[sdx]+"l"+Selections[sdx]+"_"+Histograms[hdx]+"_"+Samples[fdx]+"_log.pdf");
+			}
+		      else
+			{
+			  canvas->SaveAs(Modules[sdx]+"l"+Selections[sdx]+"_"+Histograms[hdx]+"_"+Samples[fdx]+".pdf");
+			}
+		}	
+	    }
 	}
     }
 }
+
