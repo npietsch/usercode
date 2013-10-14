501e542d7d568102c8ab54ced0f19df25b35fbd2
npietsch <>
2013-10-14 17:37:19 +0000

    update

--- Results/PU70.C
+++ Results/PU70.C
@@ -7,16 +7,16 @@ vector<TString> Names;
 
 void PU70() {
 
-//    Names.push_back("TTJetsFall11");
+//    Names.push_back("MC_PUDist_Summer11_TTJets_TuneZ2_7TeV_madgraph_tauola.root");
 //   Names.push_back("WJetsHT");
 //   Names.push_back("WJetsHT250");
 //   Names.push_back("WJetsHT300");
-   Names.push_back("LM3");
-   Names.push_back("LM6");
-   Names.push_back("LM8");
-   Names.push_back("LM13");
+//    Names.push_back("LM3");
+//    Names.push_back("LM6");
+//    Names.push_back("LM8");
+//    Names.push_back("LM13");
 //   Names.push_back("QCD");
-//   Names.push_back("SingleTop");
+  Names.push_back("SingleTop");
 //   Names.push_back("ZJets");
 
   for(int sdx=0; sdx<(int)Names.size(); ++sdx)
@@ -34,7 +34,7 @@ void PU70() {
  
       nPU_new->Draw();
       
-      TFile* file_new = new TFile(Names[sdx]+".root","RECREATE");
+      TFile* file_new = new TFile(Names[sdx]+"_new.root","RECREATE");
       nPU_new->Write();
       file_new->Close();
     }
--- Results/PU_Hannes.C
+++ Results/PU_Hannes.C
@@ -6,7 +6,7 @@
 
 void PU_Hannes() {
   //TDirectory* keep = gDirectory->GetDirectory("");
-  TFile* ORGPUDIST = new TFile("Data_PUDist_sysNo_68000_2011Full.root");
+  TFile* ORGPUDIST = new TFile("Data_PUDist_sysNo_73500_2011Full.root");
 
   cout<<"file read"<<endl;
 
@@ -22,10 +22,12 @@ void PU_Hannes() {
 
   for(int i=1; i<=orgpu->GetXaxis()->GetNbins(); i++) {
     cout<<i<<"    "<<orgpu->GetBinContent(i)<<endl;
-    rebpu->SetBinContent(i,orgpu->GetBinContent(i+1));
-    rebpu->SetBinError(i,orgpu->GetBinError(i+1));
+    rebpu->SetBinContent(i,orgpu->GetBinContent(i));
+    rebpu->SetBinError(i,orgpu->GetBinError(i));
   }
-  for(int i2=orgpu->GetXaxis()->GetNbins()+1; i2<=rebpu->GetXaxis()->GetNbins(); i2++) {
+  rebpu->SetBinContent(49, 0.);
+
+  for(int i2=orgpu->GetXaxis()->GetNbins(); i2<=rebpu->GetXaxis()->GetNbins(); i2++) {
     cout<<i2<<"    "<<0<<endl;
     
     rebpu->SetBinContent(i2,0);
@@ -38,7 +40,7 @@ void PU_Hannes() {
 
    rebpu->Draw();
 
-   TFile* REBPUDIST = new TFile("PU_Data.root","RECREATE");
+   TFile* REBPUDIST = new TFile("PU_Data_73500_new.root","RECREATE");
    rebpu->Write();
    REBPUDIST->Close();
    //   ORGPUDIST->Close();
